464c049b90c4548b3512ad347acb89de5484d919
EDbarvinsky <eugenebarvinsky@gmail.com>
2019-08-16 16:12:03 +0300

    Add default buildSystemRootDir value if system variable does not present

--- gradlew.bat
+++ gradlew.bat
@@ -2,34 +2,23 @@
 
 set GRADLE_PROPERTIES=gradle.properties
 set ERROR_MSG=%GRADLE_PROPERTIES% file is missing
-if exist %GRADLE_PROPERTIES% goto readproperies
+if exist %GRADLE_PROPERTIES% goto readproperties
 goto fail
 
-:readproperies
-For /F "tokens=1* delims==" %%A IN (gradle.properties) DO (
-    IF "%%A"=="buildSystemVersion" set buildSystemVersion=%%B
-    )
-set ERROR_MSG=buildSystemVersion is NOT defined in %GRADLE_PROPERTIES% file
-if "%buildSystemVersion%"=="" goto fail
+:readproperties
+call :setpropertyvalue buildSystemVersion , buildSystemVersion
 
-goto checkpath
+:checkpath
+if not exist "%BUILD_SYSTEM_ROOT%" (
+    call :setpropertyvalue buildSystemRootDir , BUILD_SYSTEM_ROOT
+)
+set GRADLEW_PATH=%BUILD_SYSTEM_ROOT%\%buildSystemVersion%\gradlew.bat
+if exist "%GRADLEW_PATH%" goto call
+set ERROR_MSG=Couldn't find %GRADLEW_PATH%. Check BUILD_SYSTEM_ROOT and buildSystemVersion set correctly
 
 :fail
 echo.
 echo ERROR: %ERROR_MSG%
-echo.
-echo Please check Build System guide:
-echo https://Vividus-BuildSystem
-exit /b 1
-
-:checkpath
-if not exist "%BUILD_SYSTEM_ROOT%" (
-    set BUILD_SYSTEM_ROOT=vividus-build-system)
-set GRADLEW_PATH=%BUILD_SYSTEM_ROOT%\%buildSystemVersion%\gradlew.bat
-if exist "%GRADLEW_PATH%" goto call
-echo.
-echo ERROR: Couldn't find %GRADLEW_PATH%
-echo Check BUILD_SYSTEM_ROOT and buildSystemVersion set correctly
 echo.
 echo Please check Build System guide:
 echo https://github.com/vividus-framework/vividus-build-system
@@ -39,3 +28,9 @@ exit /b 1
 
 :call
 @CALL "%GRADLEW_PATH%" %*
+exit /b 0
+
+:setpropertyvalue
+For /F "tokens=1* delims==" %%A IN (%GRADLE_PROPERTIES%) DO (
+    IF %%A==%~1 set %~2=%%B
+)
