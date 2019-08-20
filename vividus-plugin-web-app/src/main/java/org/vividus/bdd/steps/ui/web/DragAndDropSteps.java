044c6c3dc914029a5ffb614e8d16b8603dd6dba4
Uladzislau Arlouski <uarlouski@gmail.com>
2019-08-20 07:49:24 +0300

    Add step When I drag element located 'origin' and drop it location of an element located 'target'

---
+++ vividus-plugin-web-app/src/main/java/org/vividus/bdd/steps/ui/web/DragAndDropSteps.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.bdd.steps.ui.web;
+
+import javax.inject.Inject;
+
+import org.apache.commons.lang3.ObjectUtils;
+import org.jbehave.core.annotations.When;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.vividus.bdd.steps.ui.web.model.Location;
+import org.vividus.bdd.steps.ui.web.validation.IBaseValidations;
+import org.vividus.selenium.IWebDriverProvider;
+import org.vividus.ui.web.action.search.SearchAttributes;
+
+public class DragAndDropSteps
+{
+    @Inject private IBaseValidations baseValidations;
+    @Inject private IWebDriverProvider webDriverProvider;
+
+    /**
+     * Drags the <b>origin</b> element and moves it relatively to the <b>target</b> element in
+     * accordance to provided <b>location</b>.
+     * <br>
+     * <i>Example</i>
+     * <br>
+     * <code>When I drag element located `By.xpath(//div[@class='origin'])` and drop it at RIGHT_TOP of element
+     * located `By.xpath(//div[@class='target'])`</code>
+     * @param origin element to be dragged
+     * @param location location relatively to the <b>target</b> element (<b>TOP</b>,<b>BOTTOM</b>,<b>LEFT</b>,
+     * <b>RIGHT</b>,<b>CENTER</b>,<b>LEFT_TOP</b>,<b>RIGHT_TOP</b>,<b>LEFT_BOTTOM</b>,<b>RIGHT_BOTTOM</b>)
+     * @param target target element
+     */
+    @When("I drag element located `$origin` and drop it at $location of element located `$target`")
+    public void dragAndDropToTargetAtLocation(SearchAttributes origin, Location location, SearchAttributes target)
+    {
+        WebElement originElement = baseValidations.assertIfElementExists("Origin element", origin);
+        WebElement targetElement = baseValidations.assertIfElementExists("Target element", target);
+
+        if (ObjectUtils.allNotNull(originElement, targetElement))
+        {
+            Point offsetPoint = location.getPoint(originElement.getRect(), targetElement.getRect());
+            new Actions(webDriverProvider.get())
+                .dragAndDropBy(originElement, offsetPoint.getX(), offsetPoint.getY())
+                .perform();
+        }
+    }
+}
---
+++ vividus-plugin-web-app/src/test/java/org/vividus/bdd/steps/ui/web/DragAndDropStepsTests.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.bdd.steps.ui.web;
+
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openqa.selenium.Rectangle;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Interactive;
+import org.vividus.bdd.steps.ui.web.model.Location;
+import org.vividus.bdd.steps.ui.web.validation.IBaseValidations;
+import org.vividus.selenium.IWebDriverProvider;
+import org.vividus.ui.web.action.search.SearchAttributes;
+
+@ExtendWith(MockitoExtension.class)
+public class DragAndDropStepsTests
+{
+    private static final String ACTIONS_SEQUENCE = "{id=default mouse, type=pointer, parameters={pointerType=mouse}, "
+            + "actions=["
+            + "{duration=100, x=0, y=0, type=pointerMove, origin=Mock for WebElement, hashCode: %s}, "
+            + "{button=0, type=pointerDown}, "
+            + "{duration=250, x=100, y=50, type=pointerMove, origin=pointer}, "
+            + "{button=0, type=pointerUp}]}";
+
+    @Mock
+    private IBaseValidations baseValidations;
+
+    @Mock
+    private IWebDriverProvider webDriverProvider;
+
+    @Mock
+    private SearchAttributes searchAttributes;
+
+    @InjectMocks
+    private DragAndDropSteps dragAndDropSteps;
+
+    @Test
+    void testDragAndDropToTargetAtLocation()
+    {
+        WebElement origin = mockRect(100, 100, 50, 50);
+        mockFindElements(origin, mockRect(200, 200, 50, 50));
+        WebDriver driver = mock(WebDriver.class, withSettings().extraInterfaces(Interactive.class));
+        when(webDriverProvider.get()).thenReturn(driver);
+        dragAndDropSteps.dragAndDropToTargetAtLocation(searchAttributes, Location.TOP, searchAttributes);
+        verify((Interactive) driver).perform(argThat(arg -> arg.iterator().next().encode().toString()
+                .equals(String.format(ACTIONS_SEQUENCE, origin.hashCode()))));
+    }
+
+    private static WebElement mockRect(int x, int y, int height, int width)
+    {
+        WebElement element = mock(WebElement.class);
+        when(element.getRect()).thenReturn(new Rectangle(x, y, height, width));
+        return element;
+    }
+
+    static Stream<Arguments> getElements()
+    {
+        return Stream.of(
+            Arguments.of(null, null),
+            Arguments.of(null, mock(WebElement.class)),
+            Arguments.of(mock(WebElement.class), null)
+        );
+    }
+
+    @MethodSource("getElements")
+    @ParameterizedTest
+    void testDragAndDropToTargetAtLocationElementsNotFound(WebElement origin, WebElement target)
+    {
+        mockFindElements(origin, target);
+        dragAndDropSteps.dragAndDropToTargetAtLocation(searchAttributes, Location.TOP, searchAttributes);
+        verifyZeroInteractions(webDriverProvider);
+    }
+
+    void mockFindElements(WebElement origin, WebElement target)
+    {
+        lenient().when(baseValidations.assertIfElementExists("Origin element", searchAttributes)).thenReturn(origin);
+        lenient().when(baseValidations.assertIfElementExists("Target element", searchAttributes)).thenReturn(target);
+    }
+}
