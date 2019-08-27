0af7af7757521768d0b43b84e2f2bf3e336a194f
EDbarvinsky <eugenebarvinsky@gmail.com>
2019-08-27 17:00:11 +0300

    Add option to skip scenario steps after the first failed assertion

---
+++ vividus/src/main/java/org/vividus/softassert/AssertionManager.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.softassert;
+
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import org.vividus.softassert.event.AssertionFailedEvent;
+
+public class AssertionManager
+{
+    private final EventBus eventBus;
+    private ISoftAssert softAssert;
+
+    private boolean failFast;
+
+    public AssertionManager(EventBus eventBus, ISoftAssert softAssert)
+    {
+        this.eventBus = eventBus;
+        this.softAssert = softAssert;
+        eventBus.register(this);
+    }
+
+    @Subscribe
+    public void onAssertionFailure(@SuppressWarnings("unused") AssertionFailedEvent event)
+    {
+        if (failFast)
+        {
+            // Way to order EventBus listeners: all AssertionFailedEvent listeners and all events spawned by
+            // these listeners must be processed, only after that verification may be triggered
+            eventBus.post(new VerifyAssertionsEvent());
+        }
+    }
+
+    @Subscribe
+    public void onVerifyAssertions(@SuppressWarnings("unused") VerifyAssertionsEvent event)
+    {
+        softAssert.verify();
+    }
+
+    public void setFailFast(boolean failFast)
+    {
+        this.failFast = failFast;
+    }
+
+    private static class VerifyAssertionsEvent
+    {
+    }
+}
---
+++ vividus/src/test/java/org/vividus/softassert/AssertionManagerTests.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.softassert;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import com.google.common.eventbus.EventBus;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.vividus.softassert.event.AssertionFailedEvent;
+
+class AssertionManagerTests
+{
+    @ParameterizedTest
+    @CsvSource({"true, 1", "false, 0"})
+    void shouldFailFast(boolean failFast, int numberOfVerifyInvocations)
+    {
+        ISoftAssert softAssert = mock(SoftAssert.class);
+        AssertionManager assertionManager = new AssertionManager(new EventBus(), softAssert);
+        assertionManager.setFailFast(failFast);
+        assertionManager.onAssertionFailure(new AssertionFailedEvent());
+        verify(softAssert, times(numberOfVerifyInvocations)).verify();
+    }
+}
