ee3e9e294f023557775b28aeeb1eeab79219a6ff
Valery Yatsynovich <valery_yatsynovich@epam.com>
2019-12-24 16:26:53 +0300

    [bdd-engine] Make MERGING transformer applicable to table with body (#232)

--- vividus-bdd-engine/src/test/java/org/vividus/bdd/transformer/MergingTableTransformerTests.java
+++ vividus-bdd-engine/src/test/java/org/vividus/bdd/transformer/MergingTableTransformerTests.java
@@ -40,12 +40,12 @@ class MergingTableTransformerTests
     private static final String MERGE_MODE = "mergeMode";
     private static final String ROWS = "rows";
     private static final String COLUMNS = "columns";
-    private static final String PATH_ONE = "/test1.table";
-    private static final String PATH_TWO = "/test2.table";
-    private static final String PATH_THREE = "/test3.table";
+    private static final String PATH_1 = "/test1.table";
+    private static final String PATH_2 = "/test2.table";
+    private static final String PATH_3 = "/test3.table";
     private static final String TABLES_DELIMITER = "; ";
-    private static final String TWO_PATHS = String.join(TABLES_DELIMITER, PATH_ONE, PATH_TWO);
-    private static final String THREE_PATHS = String.join(TABLES_DELIMITER, PATH_ONE, PATH_TWO, PATH_THREE);
+    private static final String TWO_PATHS = String.join(TABLES_DELIMITER, PATH_1, PATH_2);
+    private static final String THREE_PATHS = String.join(TABLES_DELIMITER, PATH_1, PATH_2, PATH_3);
     private static final String TRANSFORMER = "{transformer=FROM_EXCEL, path=test.csv, sheet=test, ";
     private static final String TRANSFORMER_ONE = TRANSFORMER + "range=A2:A4, column=country}";
     private static final String TRANSFORMER_TWO = TRANSFORMER + "range=B2:B4, column=capital}";
@@ -54,6 +54,19 @@ class MergingTableTransformerTests
     private static final String HEADER_SEPARATOR = "headerSeparator";
     private static final String VALUE_SEPARATOR = "valueSeparator";
     private static final String SEPARATOR = "!";
+
+    private static final String TABLE_1_FOR_COLUMNS_MERGE = "|var6|var5|\n|b|a|\n|d|c|";
+    private static final String TABLE_2_FOR_COLUMNS_MERGE = "|var3|var4|\n|e|f|\n|g|h|";
+    private static final String TABLE_3_FOR_COLUMNS_MERGE = "|var1|var2|\n|j|i|\n|l|k|";
+    private static final String MERGED_TABLE_COLUMNS = "|var1|var2|var3|var4|var5|var6|\n|j|i|e|f|a|b|\n|l|k|g|h|c|d|";
+
+    private static final String TABLE_1_FOR_ROWS_MERGE = "|var2|var1|\n|b|a|\n|d|c|";
+    private static final String TABLE_2_FOR_ROWS_MERGE = "|var1|var2|\n|e|f|\n|g|h|";
+    private static final String TABLE_3_FOR_ROWS_MERGE = "|var1|var2|\n|i|j|\n|a|b|";
+    private static final String MERGED_TABLE_ROWS = "|var1|var2|\n|a|b|\n|c|d|\n|e|f|\n|g|h|\n|i|j|\n|a|b|";
+
+    private static final String ERROR_MORE_THAN_ONE_TABLE_PATHS = "Please, specify more than one unique table paths";
+    private static final String ERROR_AT_LEAST_ONE_TABLE_PATH = "Please, specify at least one table path";
 
     @Mock
     private ExamplesTableFactory factory;
@@ -77,7 +90,37 @@ class MergingTableTransformerTests
     void testNotUniqueTablePaths()
     {
         Properties properties = createProperties(ROWS, "/test.table; /test.table");
-        verifyIllegalArgumentException(properties, "Please, specify more than one unique table paths");
+        verifyIllegalArgumentException(properties, ERROR_MORE_THAN_ONE_TABLE_PATHS);
+    }
+
+    @Test
+    void testNoTablesToMerge()
+    {
+        Properties properties = new Properties();
+        properties.setProperty(MERGE_MODE, ROWS);
+        verifyIllegalArgumentException(properties, ERROR_MORE_THAN_ONE_TABLE_PATHS);
+    }
+
+    @Test
+    void testNoTablesToMergeHavingTableBody()
+    {
+        Properties properties = new Properties();
+        properties.setProperty(MERGE_MODE, ROWS);
+        verifyIllegalArgumentException(TABLE_1_FOR_ROWS_MERGE, properties, ERROR_AT_LEAST_ONE_TABLE_PATH);
+    }
+
+    @Test
+    void testNotEnoughTablesToMerge()
+    {
+        Properties properties = createProperties(ROWS, "");
+        verifyIllegalArgumentException(properties, ERROR_MORE_THAN_ONE_TABLE_PATHS);
+    }
+
+    @Test
+    void testNotEnoughTablesToMergeHavingTableBody()
+    {
+        Properties properties = createProperties(ROWS, "");
+        verifyIllegalArgumentException(TABLE_1_FOR_ROWS_MERGE, properties, ERROR_AT_LEAST_ONE_TABLE_PATH);
     }
 
     @Test
@@ -85,8 +128,8 @@ class MergingTableTransformerTests
     {
         Properties properties = createProperties(ROWS, TWO_PATHS);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var1|var2|\n|a|b|");
-        mockCreateExamplesTable(PATH_TWO, "|var1|var3|\n|c|d|");
+        mockCreateExamplesTable(PATH_1, "|var1|var2|\n|a|b|");
+        mockCreateExamplesTable(PATH_2, "|var1|var3|\n|c|d|");
         verifyIllegalArgumentException(properties, "Please, specify tables with the same sets of headers");
     }
 
@@ -95,8 +138,8 @@ class MergingTableTransformerTests
     {
         Properties properties = createProperties(COLUMNS, TWO_PATHS);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var1|var2|\n|a|b|\n|c|d|");
-        mockCreateExamplesTable(PATH_TWO, "|var3|var4|\n|e|f|");
+        mockCreateExamplesTable(PATH_1, "|var1|var2|\n|a|b|\n|c|d|");
+        mockCreateExamplesTable(PATH_2, "|var3|var4|\n|e|f|");
         verifyIllegalArgumentException(properties, "Please, specify tables with the same number of rows");
     }
 
@@ -105,8 +148,8 @@ class MergingTableTransformerTests
     {
         Properties properties = createProperties(COLUMNS, TWO_PATHS);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var2|var1|\n|b|a|");
-        mockCreateExamplesTable(PATH_TWO, "|var2|var3|\n|c|d|");
+        mockCreateExamplesTable(PATH_1, "|var2|var1|\n|b|a|");
+        mockCreateExamplesTable(PATH_2, "|var2|var3|\n|c|d|");
         verifyIllegalArgumentException(properties, "Please, specify tables with the unique sets of headers");
     }
 
@@ -115,10 +158,21 @@ class MergingTableTransformerTests
     {
         Properties properties = createProperties(ROWS, THREE_PATHS);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var2|var1|\n|b|a|\n|d|c|");
-        mockCreateExamplesTable(PATH_TWO, "|var1|var2|\n|e|f|\n|g|h|");
-        mockCreateExamplesTable(PATH_THREE, "|var1|var2|\n|i|j|\n|a|b|");
-        assertMerge(properties, "|var1|var2|\n|a|b|\n|c|d|\n|e|f|\n|g|h|\n|i|j|\n|a|b|");
+        mockCreateExamplesTable(PATH_1, TABLE_1_FOR_ROWS_MERGE);
+        mockCreateExamplesTable(PATH_2, TABLE_2_FOR_ROWS_MERGE);
+        mockCreateExamplesTable(PATH_3, TABLE_3_FOR_ROWS_MERGE);
+        assertMerge(properties, MERGED_TABLE_ROWS);
+    }
+
+    @Test
+    void testTransformInRowsModeHavingTableBody()
+    {
+        Properties properties = createProperties(ROWS, TWO_PATHS);
+        when(configuration.examplesTableFactory()).thenReturn(factory);
+        mockCreateExamplesTable(PATH_1, TABLE_1_FOR_ROWS_MERGE);
+        mockCreateExamplesTable(PATH_2, TABLE_2_FOR_ROWS_MERGE);
+        mockCreateExamplesTable(TABLE_3_FOR_ROWS_MERGE);
+        assertMerge(TABLE_3_FOR_ROWS_MERGE, properties, MERGED_TABLE_ROWS);
     }
 
     @Test
@@ -126,10 +180,21 @@ class MergingTableTransformerTests
     {
         Properties properties = createProperties(COLUMNS, THREE_PATHS);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var6|var5|\n|b|a|\n|d|c|");
-        mockCreateExamplesTable(PATH_TWO, "|var3|var4|\n|e|f|\n|g|h|");
-        mockCreateExamplesTable(PATH_THREE, "|var1|var2|\n|j|i|\n|l|k|");
-        assertMerge(properties, "|var1|var2|var3|var4|var5|var6|\n|j|i|e|f|a|b|\n|l|k|g|h|c|d|");
+        mockCreateExamplesTable(PATH_1, TABLE_1_FOR_COLUMNS_MERGE);
+        mockCreateExamplesTable(PATH_2, TABLE_2_FOR_COLUMNS_MERGE);
+        mockCreateExamplesTable(PATH_3, TABLE_3_FOR_COLUMNS_MERGE);
+        assertMerge(properties, MERGED_TABLE_COLUMNS);
+    }
+
+    @Test
+    void testTransformsInColumnsModeHavingTableBody()
+    {
+        Properties properties = createProperties(COLUMNS, TWO_PATHS);
+        when(configuration.examplesTableFactory()).thenReturn(factory);
+        mockCreateExamplesTable(PATH_1, TABLE_1_FOR_COLUMNS_MERGE);
+        mockCreateExamplesTable(PATH_2, TABLE_2_FOR_COLUMNS_MERGE);
+        mockCreateExamplesTable(TABLE_3_FOR_COLUMNS_MERGE);
+        assertMerge(TABLE_3_FOR_COLUMNS_MERGE, properties, MERGED_TABLE_COLUMNS);
     }
 
     @Test
@@ -160,9 +225,9 @@ class MergingTableTransformerTests
         Properties properties = createProperties(COLUMNS, THREE_PATHS);
         properties.setProperty(FILLER_VALUE_PARAMETER_NAME, FILLER_VALUE_PARAMETER_VALUE);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var1|var2|\n|a|b|\n|c|d|\n|e|f|");
-        mockCreateExamplesTable(PATH_TWO, "|var5|\n|i|\n|j|");
-        mockCreateExamplesTable(PATH_THREE, "|var3|var4|\n|g|h|");
+        mockCreateExamplesTable(PATH_1, "|var1|var2|\n|a|b|\n|c|d|\n|e|f|");
+        mockCreateExamplesTable(PATH_2, "|var5|\n|i|\n|j|");
+        mockCreateExamplesTable(PATH_3, "|var3|var4|\n|g|h|");
         assertMerge(properties,
                 "|var1|var2|var3|var4|var5|\n|a|b|g|h|i|\n|c|d|value|value|j|\n|e|f|value|value|value|");
     }
@@ -173,9 +238,9 @@ class MergingTableTransformerTests
         Properties properties = createProperties(ROWS, THREE_PATHS);
         properties.setProperty(FILLER_VALUE_PARAMETER_NAME, FILLER_VALUE_PARAMETER_VALUE);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "|var1|var3|\n|e|f|");
-        mockCreateExamplesTable(PATH_TWO, "|var1|var2|\n|b|a|\n|d|c|");
-        mockCreateExamplesTable(PATH_THREE, "|var1|var2|var3|\n|r|g|h|");
+        mockCreateExamplesTable(PATH_1, "|var1|var3|\n|e|f|");
+        mockCreateExamplesTable(PATH_2, "|var1|var2|\n|b|a|\n|d|c|");
+        mockCreateExamplesTable(PATH_3, "|var1|var2|var3|\n|r|g|h|");
         assertMerge(properties, "|var1|var2|var3|\n|e|value|f|\n|b|a|value|\n|d|c|value|\n|r|g|h|");
     }
 
@@ -187,8 +252,8 @@ class MergingTableTransformerTests
         properties.setProperty(HEADER_SEPARATOR, SEPARATOR);
         properties.setProperty(VALUE_SEPARATOR, SEPARATOR);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "{headerSeparator=!,valueSeparator=!}\n!var1!var2!\n!A!B|C!");
-        mockCreateExamplesTable(PATH_TWO, "{headerSeparator=!,valueSeparator=!}\n!var1!\n!D|E!");
+        mockCreateExamplesTable(PATH_1, "{headerSeparator=!,valueSeparator=!}\n!var1!var2!\n!A!B|C!");
+        mockCreateExamplesTable(PATH_2, "{headerSeparator=!,valueSeparator=!}\n!var1!\n!D|E!");
         assertMerge(properties, "!var1!var2!\n!A!B|C!\n!D|E!value!");
     }
 
@@ -200,20 +265,30 @@ class MergingTableTransformerTests
         properties.setProperty(HEADER_SEPARATOR, SEPARATOR);
         properties.setProperty(VALUE_SEPARATOR, SEPARATOR);
         when(configuration.examplesTableFactory()).thenReturn(factory);
-        mockCreateExamplesTable(PATH_ONE, "{headerSeparator=!,valueSeparator=!}\n!var1!var3!\n!B!A|E!");
-        mockCreateExamplesTable(PATH_TWO, "{headerSeparator=!,valueSeparator=!}\n!var2!\r\n!D|E!\n!F!");
+        mockCreateExamplesTable(PATH_1, "{headerSeparator=!,valueSeparator=!}\n!var1!var3!\n!B!A|E!");
+        mockCreateExamplesTable(PATH_2, "{headerSeparator=!,valueSeparator=!}\n!var2!\r\n!D|E!\n!F!");
         assertMerge(properties, "!var1!var2!var3!\n!B!D|E!A|E!\n!value!F!value!");
     }
 
-    private void mockCreateExamplesTable(String path, String table)
+    private void mockCreateExamplesTable(String table)
     {
-        lenient().when(factory.createExamplesTable(path)).thenReturn(new ExamplesTable(table));
+        mockCreateExamplesTable(table, table);
+    }
+
+    private void mockCreateExamplesTable(String input, String table)
+    {
+        lenient().when(factory.createExamplesTable(input)).thenReturn(new ExamplesTable(table));
     }
 
     private void assertMerge(Properties properties, String expected)
     {
+        assertMerge(StringUtils.EMPTY, properties, expected);
+    }
+
+    private void assertMerge(String tableBody, Properties properties, String expected)
+    {
         ExamplesTableProperties examplesTableProperties = new ExamplesTableProperties(properties);
-        assertEquals(expected, mergingTableTransformer.transform(StringUtils.EMPTY, examplesTableProperties));
+        assertEquals(expected, mergingTableTransformer.transform(tableBody, examplesTableProperties));
     }
 
     private Properties createProperties(String mergeMode, String tables)
@@ -226,8 +301,14 @@ class MergingTableTransformerTests
 
     private void verifyIllegalArgumentException(Properties properties, String message)
     {
+        String tableBody = StringUtils.EMPTY;
+        verifyIllegalArgumentException(tableBody, properties, message);
+    }
+
+    private void verifyIllegalArgumentException(String tableBody, Properties properties, String message)
+    {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
-            () -> mergingTableTransformer.transform(StringUtils.EMPTY, new ExamplesTableProperties(properties)));
+            () -> mergingTableTransformer.transform(tableBody, new ExamplesTableProperties(properties)));
         assertEquals(message, exception.getMessage());
     }
 }
