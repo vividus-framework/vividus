vividus-framework/vividus

commit e083434176737f98a640e166cb9d1f983535ada9

Add ability to configure meta filters and story timeout per batch (#286)

vividus-bdd-engine/src/main/java/org/vividus/bdd/context/BddRunContext.java
@@ -22,15 +22,15 @@ import java.util.Optional;
 
 import org.jbehave.core.embedder.PerformableTree.Status;
 import org.jbehave.core.model.Story;
-import org.vividus.bdd.IBatchStorage;
+import org.vividus.bdd.batch.BatchResourceConfiguration;
+import org.vividus.bdd.batch.BatchStorage;
 import org.vividus.bdd.model.RunningStory;
-import org.vividus.bdd.resource.ResourceBatch;
 import org.vividus.testcontext.TestContext;
 
 public class BddRunContext implements IBddRunContext
 {
     private TestContext testContext;
-    private IBatchStorage batchStorage;
+    private BatchStorage batchStorage;
 
     private Optional<String> runningBatchKey;
     private boolean dryRun;
@@ -96,9 +96,9 @@ public class BddRunContext implements IBddRunContext
     }
 
     @Override
-    public ResourceBatch getRunningBatch()
+    public BatchResourceConfiguration getRunningBatch()
     {
-        return batchStorage.getBatch(getRunningBatchKey());
+        return batchStorage.getBatchResourceConfiguration(getRunningBatchKey());
     }
 
     @Override
@@ -134,7 +134,7 @@ public class BddRunContext implements IBddRunContext
         this.testContext = testContext;
     }
 
-    public void setBatchStorage(IBatchStorage batchStorage)
+    public void setBatchStorage(BatchStorage batchStorage)
     {
         this.batchStorage = batchStorage;
     }

vividus-bdd-engine/src/test/java/org/vividus/bdd/context/BddRunContextTests.java
@@ -30,9 +30,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.vividus.bdd.IBatchStorage;
+import org.vividus.bdd.batch.BatchResourceConfiguration;
+import org.vividus.bdd.batch.BatchStorage;
 import org.vividus.bdd.model.RunningStory;
-import org.vividus.bdd.resource.ResourceBatch;
 import org.vividus.testcontext.SimpleTestContext;
 
 @ExtendWith(MockitoExtension.class)
@@ -41,7 +41,7 @@ class BddRunContextTests
     private static final String BATCH_KEY = "batchKey";
 
     @Mock
-    private IBatchStorage batchStorage;
+    private BatchStorage batchStorage;
 
     @InjectMocks
     private BddRunContext bddRunContext;
@@ -100,8 +100,8 @@ class BddRunContextTests
     {
         String batchKey = BATCH_KEY;
         bddRunContext.putRunningBatch(batchKey);
-        ResourceBatch batch = new ResourceBatch();
-        when(batchStorage.getBatch(batchKey)).thenReturn(batch);
+        BatchResourceConfiguration batch = new BatchResourceConfiguration();
+        when(batchStorage.getBatchResourceConfiguration(batchKey)).thenReturn(batch);
         assertEquals(batch, bddRunContext.getRunningBatch());
     }
 

vividus-bdd-engine/src/main/java/org/vividus/bdd/BatchStorage.java
@@ -1,62 +1,0 @@
-/*
- * Copyright 2019-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.vividus.bdd;
-
-import java.io.IOException;
-import java.util.Comparator;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
-
-import org.apache.commons.lang3.StringUtils;
-import org.vividus.bdd.resource.ResourceBatch;
-import org.vividus.util.property.IPropertyMapper;
-
-public class BatchStorage implements IBatchStorage
-{
-    private static final String BATCH = "batch-";
-    private static final Pattern FORMAT_PATTERN = Pattern.compile(BATCH + "\\d+");
-
-    private final Map<String, ResourceBatch> batches;
-
-    public BatchStorage(IPropertyMapper propertyMapper, String propertyPrefix) throws IOException
-    {
-        batches = propertyMapper.readValues(propertyPrefix, ResourceBatch.class).entrySet().stream()
-                .filter(e -> FORMAT_PATTERN.matcher(e.getKey()).matches())
-                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (v1, v2) -> null,
-                    () -> new TreeMap<>(Comparator.comparingInt(BatchStorage::getBatchNumber))));
-    }
-
-    private static int getBatchNumber(String batchName)
-    {
-        return Integer.parseInt(StringUtils.removeStart(batchName, BATCH));
-    }
-
-    @Override
-    public ResourceBatch getBatch(String batchKey)
-    {
-        return getBatches().get(batchKey);
-    }
-
-    @Override
-    public Map<String, ResourceBatch> getBatches()
-    {
-        return batches;
-    }
-}

vividus-bdd-engine/src/main/java/org/vividus/bdd/BatchedEmbedder.java
@@ -22,21 +22,36 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Optional;
 
+import org.jbehave.core.configuration.Configuration;
+import org.jbehave.core.embedder.Embedder;
 import org.jbehave.core.embedder.EmbedderControls;
+import org.jbehave.core.embedder.EmbedderMonitor;
 import org.jbehave.core.embedder.MetaFilter;
 import org.jbehave.core.embedder.PerformableTree;
 import org.jbehave.core.embedder.StoryManager;
 import org.jbehave.core.failures.BatchFailures;
+import org.jbehave.core.steps.InjectableStepsFactory;
+import org.vividus.bdd.batch.BatchExecutionConfiguration;
+import org.vividus.bdd.batch.BatchStorage;
 import org.vividus.bdd.context.BddRunContext;
 
-public class BatchedEmbedder extends ExtendedEmbedder
+public class BatchedEmbedder extends Embedder
 {
-    private BddRunContext bddRunContext;
+    private final BddRunContext bddRunContext;
+    private final BatchStorage batchStorage;
 
-    private String batch;
     private boolean ignoreFailureInBatches;
     private boolean reportBeforeStories = true;
     private boolean reportAfterStories;
+    private boolean generateViewAfterBatches;
+
+    private String batch;
+
+    public BatchedEmbedder(BddRunContext bddRunContext, BatchStorage batchStorage)
+    {
+        this.bddRunContext = bddRunContext;
+        this.batchStorage = batchStorage;
+    }
 
     public void runStoriesAsPaths(Map<String, List<String>> storyPathsBatches)
     {
@@ -52,6 +67,11 @@ public class BatchedEmbedder extends ExtendedEmbedder
 
                 Entry<String, List<String>> storyPathsBatch = iterator.next();
                 batch = storyPathsBatch.getKey();
+
+                BatchExecutionConfiguration batchExecutionConfiguration = batchStorage.getBatchExecutionConfiguration(
+                        batch);
+                useEmbedderControls(createEmbedderControls(batchExecutionConfiguration));
+                useMetaFilters(batchExecutionConfiguration.getMetaFilters());
 
                 EmbedderControls embedderControls = embedderControls();
                 embedderMonitor.usingControls(embedderControls);
@@ -95,7 +115,7 @@ public class BatchedEmbedder extends ExtendedEmbedder
         }
         finally
         {
-            if (getEmbedderControlsProvider().isGenerateViewAfterBatches())
+            if (generateViewAfterBatches)
             {
                 generateReportsView();
             }
@@ -118,18 +138,16 @@ public class BatchedEmbedder extends ExtendedEmbedder
         return performableTree;
     }
 
-    @Override
-    public EmbedderControls embedderControls()
+    private EmbedderControls createEmbedderControls(BatchExecutionConfiguration batchExecutionConfiguration)
     {
-        IEmbedderControlsProvider embedderControlsProvider = getEmbedderControlsProvider();
-        return Optional.ofNullable(batch)
-                .map(embedderControlsProvider::get)
-                .orElseGet(embedderControlsProvider::getDefault);
-    }
-
-    public void setBddRunContext(BddRunContext bddRunContext)
-    {
-        this.bddRunContext = bddRunContext;
+        EmbedderControls embedderControls = new EmbedderControls();
+        embedderControls.useStoryTimeouts(
+                Long.toString(batchExecutionConfiguration.getStoryExecutionTimeout().toSeconds()));
+        Optional.ofNullable(batchExecutionConfiguration.getThreads())
+                .ifPresent(embedderControls::useThreads);
+        embedderControls.doIgnoreFailureInStories(true);
+        embedderControls.doGenerateViewAfterStories(false);
+        return embedderControls;
     }
 
     public void setPerformableTree(PerformableTree performableTree)
@@ -140,5 +158,25 @@ public class BatchedEmbedder extends ExtendedEmbedder
     public void setIgnoreFailureInBatches(boolean ignoreFailureInBatches)
     {
         this.ignoreFailureInBatches = ignoreFailureInBatches;
+    }
+
+    public void setConfiguration(Configuration configuration)
+    {
+        useConfiguration(configuration);
+    }
+
+    public void setStepFactory(InjectableStepsFactory stepsFactory)
+    {
+        useStepsFactory(stepsFactory);
+    }
+
+    public void setEmbedderMonitor(EmbedderMonitor embedderMonitor)
+    {
+        useEmbedderMonitor(embedderMonitor);
+    }
+
+    public void setGenerateViewAfterBatches(boolean generateViewAfterBatches)
+    {
+        this.generateViewAfterBatches = generateViewAfterBatches;
     }
 }

vividus-bdd-engine/src/main/java/org/vividus/bdd/BatchedPathFinder.java
@@ -22,31 +22,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.vividus.bdd.resource.ResourceBatch;
+import org.vividus.bdd.batch.BatchResourceConfiguration;
+import org.vividus.bdd.batch.BatchStorage;
 
 public class BatchedPathFinder implements IBatchedPathFinder
 {
     private IPathFinder pathFinder;
-    private IBatchStorage batchStorage;
+    private BatchStorage batchStorage;
+
+    public BatchedPathFinder(IPathFinder pathFinder, BatchStorage batchStorage)
+    {
+        this.pathFinder = pathFinder;
+        this.batchStorage = batchStorage;
+    }
 
     @Override
     public Map<String, List<String>> findPaths() throws IOException
     {
         Map<String, List<String>> batchedPaths = new LinkedHashMap<>();
-        for (Entry<String, ResourceBatch> batch : batchStorage.getBatches().entrySet())
+        for (Entry<String, BatchResourceConfiguration> batch : batchStorage.getBatchResourceConfigurations().entrySet())
         {
             batchedPaths.put(batch.getKey(), pathFinder.findPaths(batch.getValue()));
         }
         return batchedPaths;
-    }
-
-    public void setPathFinder(IPathFinder pathFinder)
-    {
-        this.pathFinder = pathFinder;
-    }
-
-    public void setBatchStorage(IBatchStorage batchStorage)
-    {
-        this.batchStorage = batchStorage;
     }
 }

vividus-bdd-engine/src/main/java/org/vividus/bdd/EmbedderControlsProvider.java
@@ -1,97 +1,0 @@
-/*
- * Copyright 2019-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.vividus.bdd;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.commons.lang3.math.NumberUtils;
-import org.jbehave.core.embedder.EmbedderControls;
-import org.vividus.util.property.IPropertyMapper;
-
-public class EmbedderControlsProvider implements IEmbedderControlsProvider
-{
-    private static final String PROPERTIES_PREFIX = "bdd.batch-";
-    private static final String DEFAULT_BATCH = "batch-1";
-
-    private final Map<String, Integer> threads = new HashMap<>();
-    private String storyExecutionTimeout;
-    private boolean ignoreFailureInStories;
-    private boolean generateViewAfterBatches;
-
-    private final Map<String, EmbedderControls> batchedControls = new HashMap<>();
-
-    private IPropertyMapper propertyMapper;
-
-    public void init() throws IOException
-    {
-        propertyMapper.readValues(PROPERTIES_PREFIX, Map.class)
-                .forEach((k, v) -> threads.put("batch-" + k, NumberUtils.createInteger((String) v.get("threads"))));
-    }
-
-    @Override
-    public EmbedderControls get(String batch)
-    {
-        if (batchedControls.containsKey(batch))
-        {
-            return batchedControls.get(batch);
-        }
-        EmbedderControls controls = new EmbedderControls();
-        if (threads.containsKey(batch))
-        {
-            controls.useThreads(threads.get(batch));
-        }
-        controls.useStoryTimeouts(storyExecutionTimeout);
-        controls.doIgnoreFailureInStories(ignoreFailureInStories);
-        controls.doGenerateViewAfterStories(generateViewAfterBatches);
-        batchedControls.put(batch, controls);
-        return controls;
-    }
-
-    @Override
-    public EmbedderControls getDefault()
-    {
-        return get(DEFAULT_BATCH);
-    }
-
-    @Override
-    public boolean isGenerateViewAfterBatches()
-    {
-        return generateViewAfterBatches;
-    }
-
-    public void setStoryExecutionTimeout(String storyExecutionTimeout)
-    {
-        this.storyExecutionTimeout = storyExecutionTimeout;
-    }
-
-    public void setIgnoreFailureInStories(boolean ignoreFailureInStories)
-    {
-        this.ignoreFailureInStories = ignoreFailureInStories;
-    }
-
-    public void setGenerateViewAfterBatches(boolean generateViewAfterBatches)
-    {
-        this.generateViewAfterBatches = generateViewAfterBatches;
-    }
-
-    public void setPropertyMapper(IPropertyMapper propertyMapper)
-    {
-        this.propertyMapper = propertyMapper;
-    }
-}

vividus-bdd-engine/src/main/java/org/vividus/bdd/ExtendedEmbedder.java
@@ -1,66 +1,0 @@
-/*
- * Copyright 2019-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.vividus.bdd;
-
-import java.util.List;
-
-import org.jbehave.core.configuration.Configuration;
-import org.jbehave.core.embedder.Embedder;
-import org.jbehave.core.embedder.EmbedderControls;
-import org.jbehave.core.embedder.EmbedderMonitor;
-import org.jbehave.core.steps.InjectableStepsFactory;
-
-public class ExtendedEmbedder extends Embedder
-{
-    private IEmbedderControlsProvider embedderControlsProvider;
-
-    @Override
-    public EmbedderControls embedderControls()
-    {
-        return embedderControlsProvider.getDefault();
-    }
-
-    protected IEmbedderControlsProvider getEmbedderControlsProvider()
-    {
-        return embedderControlsProvider;
-    }
-
-    public void setEmbedderControlsProvider(IEmbedderControlsProvider embedderControlsProvider)
-    {
-        this.embedderControlsProvider = embedderControlsProvider;
-    }
-
-    public void setConfiguration(Configuration configuration)
-    {
-        useConfiguration(configuration);
-    }
-
-    public void setMetaFilters(List<String> metaFilters)
-    {
-        useMetaFilters(metaFilters);
-    }
-
-    public void setStepFactory(InjectableStepsFactory stepsFactory)
-    {
-        useStepsFactory(stepsFactory);
-    }
-
-    public void setEmbedderMonitor(EmbedderMonitor embedderMonitor)
-    {
-        useEmbedderMonitor(embedderMonitor);
-    }
-}

vividus-bdd-engine/src/main/java/org/vividus/bdd/IBatchStorage.java
@@ -1,28 +1,0 @@
-/*
- * Copyright 2019-2020 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.vividus.bdd;
-
-import java.util.Map;
-
-import org.vividus.bdd.resource.ResourceBatch;
-
-public interface IBatchStorage
-{
-    ResourceBatch getBatch(String batchKey);
-
-    Map<String, ResourceBatch> getBatches();
-}

commit c5925b46a1baa7277cfb7e6025871ff3c43dcd0e

Fix NPE when stories are run using JUnit runner
jbehave-junit-runner builds JUnit execution tree before the actual
execution. Also it knows nothing about Vividus batches. As result
when BDD stories are run NPE is thrown, because no batch is running.
This is a fix for regression bug introduced in e083434 (see
EmbedderControlsProvider.getDefault()).

vividus-bdd-engine/src/main/java/org/vividus/bdd/context/BddRunContext.java
@@ -29,7 +29,8 @@ public class BddRunContext implements IBddRunContext
 {
     private TestContext testContext;
 
-    private Optional<String> runningBatchKey;
+    // must be initialized for jbehave-junit-runner
+    private Optional<String> runningBatchKey = Optional.of("batch-1");
     private boolean dryRun;
 
     public void putRunningStory(RunningStory story, boolean givenStory)

vividus-bdd-engine/src/test/java/org/vividus/bdd/context/BddRunContextTests.java
@@ -84,6 +84,12 @@ class BddRunContextTests
     }
 
     @Test
+    void testGetDefaultRunningBatchKey()
+    {
+        assertEquals("batch-1", bddRunContext.getRunningBatchKey());
+    }
+
+    @Test
     void testGetRunningBatchKey()
     {
         bddRunContext.putRunningBatch(BATCH_KEY);