c7e4ce9285063a3a711e77bf060848560316c325
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-03-04 15:45:53 +0300

    Do not scroll elements in viewport inside Cross Origin frames

--- vividus-plugin-web-app/src/main/resources/org/vividus/ui/web/action/scroll-element-into-viewport-center.js
+++ vividus-plugin-web-app/src/main/resources/org/vividus/ui/web/action/scroll-element-into-viewport-center.js
@@ -2,12 +2,19 @@ var elementToScroll = arguments[0];
 var exit = arguments[arguments.length-1];
 (scrollElementIntoViewportCenter = function() {
     var currentWindow = window;
-    while (currentWindow !== window.top) {
-        elementToScroll = currentWindow.frameElement;
-        currentWindow = currentWindow.parent;
+    try {
+        // Check, if we have an access to top level document
+        window.top.document;
+        while (currentWindow !== window.top) {
+            elementToScroll = currentWindow.frameElement;
+            currentWindow = currentWindow.parent;
+        }
+        currentWindow.scrollBy(0, elementToScroll.getBoundingClientRect().top - currentWindow.innerHeight * 0.25);
+        setTimeout(scrollElementIntoViewportCenter, 500);
     }
-    currentWindow.scrollBy(0, elementToScroll.getBoundingClientRect().top - currentWindow.innerHeight * 0.25);
-    setTimeout(scrollElementIntoViewportCenter, 500);
+    catch(e) {
+        console.error(e, e.stack)
+    }
     exit();
 }) ();
 
