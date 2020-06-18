2cac671737008f59411ccb255e951e095b5ed86a
Nikita <grud97@gmail.com>
2020-06-18 16:47:03 +0300

    Add When I capture HTTP $httpMethod request with URL pattern `$urlPattern` and save request data to $scopes variable `$variableName`

--- vividus-plugin-web-app/src/main/java/org/vividus/proxy/steps/ProxySteps.java
+++ vividus-plugin-web-app/src/main/java/org/vividus/proxy/steps/ProxySteps.java
@@ -30,7 +30,10 @@ import javax.inject.Inject;
 
 import com.browserup.harreader.model.Har;
 import com.browserup.harreader.model.HarEntry;
+import com.browserup.harreader.model.HarPostData;
+import com.browserup.harreader.model.HarPostDataParam;
 import com.browserup.harreader.model.HarQueryParam;
+import com.browserup.harreader.model.HarRequest;
 import com.browserup.harreader.model.HttpMethod;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -46,6 +49,7 @@ import org.vividus.reporter.event.IAttachmentPublisher;
 import org.vividus.softassert.ISoftAssert;
 import org.vividus.ui.web.action.IWaitActions;
 
+@SuppressWarnings("PMD.ExcessiveImports")
 public class ProxySteps
 {
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
@@ -138,11 +142,75 @@ public class ProxySteps
         if (harEntries.size() == 1)
         {
             HarEntry harEntry = harEntries.get(0);
-            Map<String, String> queryMap = harEntry.getRequest().getQueryString()
-                    .stream()
-                    .collect(Collectors.toMap(HarQueryParam::getName, HarQueryParam::getValue));
-            bddVariableContext.putVariable(scopes, variableName, queryMap);
+            bddVariableContext.putVariable(scopes, variableName, getQueryParameters(harEntry.getRequest()));
         }
+    }
+
+    /**
+     * Saves the query string, body from request with given URL-pattern and response status from response
+     * into the variable with specified name and scopes.
+     * <p>
+     * This step requires proxy to be turned on.
+     * It can be done via setting properties or switching on <b>@proxy</b> metatag inside the story file.
+     * Step gets proxies log, extract from contained requests URLs and match them with URL-pattern
+     * If there is one entry, it saves the query string from request as Map of keys and values
+     * into the variable with specified name and scopes.
+     * If there weren't any calls or more than one matching requirements, HAR file with all
+     * calls will be attached to report.
+     * </p>
+     * @param httpMethod HTTP method to filter by
+     * @param urlPattern The string value of URL-pattern to filter by
+     * @param scopes The set (comma separated list of scopes e.g.: STORY, NEXT_BATCHES) of variable's scope<br>
+     * <i>Available scopes:</i>
+     * <ul>
+     * <li><b>STEP</b> - the variable will be available only within the step,
+     * <li><b>SCENARIO</b> - the variable will be available only within the scenario,
+     * <li><b>STORY</b> - the variable will be available within the whole story,
+     * <li><b>NEXT_BATCHES</b> - the variable will be available starting from next batch
+     * </ul>
+     * @param variableName A variable name
+     * @throws IOException If any error happens during operation
+     */
+    @When("I capture HTTP $httpMethod request with URL pattern `$urlPattern` and save request data to $scopes "
+            + "variable `$variableName`")
+    public void captureRequestAndSaveRequestData(HttpMethod httpMethod, String urlPattern, Set<VariableScope> scopes,
+            String variableName) throws IOException
+    {
+        List<HarEntry> harEntries = checkNumberOfRequests(httpMethod, urlPattern, ComparisonRule.EQUAL_TO, 1);
+        if (harEntries.size() == 1)
+        {
+            HarEntry harEntry = harEntries.get(0);
+            HarRequest request = harEntry.getRequest();
+            HarPostData postData = request.getPostData();
+            Map<String, Object> requestData = Map.of(
+                    "query", getQueryParameters(request),
+                    "requestBody", getRequestBody(postData),
+                    "requestBodyParameters", getRequestBodyParameters(postData),
+                    "responseStatus", harEntry.getResponse().getStatus()
+            );
+            bddVariableContext.putVariable(scopes, variableName, requestData);
+        }
+    }
+
+    private Map<String, String> getRequestBody(HarPostData postData)
+    {
+        return Map.of(
+                "mimeType", postData.getMimeType(),
+                "text", postData.getText(),
+                "comment", String.valueOf(postData.getComment())
+        );
+    }
+
+    private Map<String, String> getRequestBodyParameters(HarPostData postData)
+    {
+        return postData.getParams().stream()
+                .collect(Collectors.toMap(HarPostDataParam::getName, HarPostDataParam::getValue));
+    }
+
+    private Map<String, String> getQueryParameters(HarRequest request)
+    {
+        return request.getQueryString().stream()
+                .collect(Collectors.toMap(HarQueryParam::getName, HarQueryParam::getValue));
     }
 
     /**
