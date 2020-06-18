bc3c60fcde67ecd845b45449794f90bd74be7651
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-06-18 22:19:40 +0300

    [bdd-engine] Fix enum converters race condition (#684)

--- vividus-bdd-engine/src/test/java/org/vividus/bdd/converter/FluentTrimmedEnumConverterTests.java
+++ vividus-bdd-engine/src/test/java/org/vividus/bdd/converter/FluentTrimmedEnumConverterTests.java
@@ -18,12 +18,14 @@ package org.vividus.bdd.converter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.vividus.bdd.steps.ComparisonRule;
 
 class FluentTrimmedEnumConverterTests
 {
+    private static final String VALUE = "value";
     private final FluentTrimmedEnumConverter fluentTrimmedEnumConverter = new FluentTrimmedEnumConverter();
 
     @ValueSource(strings = {"equal to ", " equal to "})
@@ -32,5 +34,75 @@ class FluentTrimmedEnumConverterTests
     {
         assertEquals(ComparisonRule.EQUAL_TO,
                 fluentTrimmedEnumConverter.convertValue(valueToConvert, ComparisonRule.class));
+    }
+
+    @Test
+    void shouldUseFromStringMethod()
+    {
+        assertEquals(Convertable.VALUE_FROMSTRING, fluentTrimmedEnumConverter.convertValue(VALUE, Convertable.class));
+    }
+
+    @Test
+    void shouldFallBackToDefaultConverterWhenNoMethodPresent()
+    {
+        assertEquals(NoMethod.VALUE, fluentTrimmedEnumConverter.convertValue(VALUE, NoMethod.class));
+    }
+
+    @Test
+    void shouldFallBackToDefaultConverterIfTheMethodNotPublic()
+    {
+        assertEquals(UnAccessibleMethod.VALUE, fluentTrimmedEnumConverter.convertValue(VALUE,
+                UnAccessibleMethod.class));
+    }
+
+    @Test
+    void shouldFallBackToDefaultConverterIfTheMethodThrowsAnException()
+    {
+        assertEquals(Throwing.VALUE, fluentTrimmedEnumConverter.convertValue(VALUE,
+                Throwing.class));
+    }
+
+    public enum Convertable
+    {
+        VALUE,
+        VALUE_FROMSTRING;
+
+        public static Convertable fromString(String toConvert)
+        {
+            return VALUE_FROMSTRING;
+        }
+    }
+
+    public enum NoMethod
+    {
+        VALUE,
+        VALUE_FROMSTRING;
+
+        public static NoMethod parse(String toConvert)
+        {
+            return VALUE_FROMSTRING;
+        }
+    }
+
+    public enum UnAccessibleMethod
+    {
+        VALUE,
+        VALUE_FROMSTRING;
+
+        protected static UnAccessibleMethod fromString(String toConvert)
+        {
+            return VALUE_FROMSTRING;
+        }
+    }
+
+    public enum Throwing
+    {
+        VALUE,
+        VALUE_FROMSTRING;
+
+        public static Throwing fromString(String toConvert)
+        {
+            throw new IllegalArgumentException("unable to convert");
+        }
     }
 }
