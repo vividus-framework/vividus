87a5707af40bbaad425837bf6dc55dd7f2f52dcd
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-09-07 10:36:27 +0300

    [plugin-web-app] Use locators for dropdown elements steps (#889)

--- vividus-plugin-web-app/src/test/java/org/vividus/bdd/steps/ui/web/DropdownStepsTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/bdd/steps/ui/web/DropdownStepsTests.java
@@ -20,7 +20,6 @@ import static java.util.Collections.singletonList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -32,20 +31,18 @@ import java.util.List;
 import org.jbehave.core.model.ExamplesTable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WrapsElement;
 import org.openqa.selenium.support.ui.Select;
 import org.vividus.bdd.steps.ui.validation.IBaseValidations;
 import org.vividus.bdd.steps.ui.validation.IDescriptiveSoftAssert;
 import org.vividus.ui.action.search.Locator;
-import org.vividus.ui.action.search.SearchParameters;
-import org.vividus.ui.action.search.Visibility;
-import org.vividus.ui.web.DropDownState;
 import org.vividus.ui.web.action.IFieldActions;
 import org.vividus.ui.web.action.IWebElementActions;
 import org.vividus.ui.web.action.search.WebLocatorType;
@@ -53,22 +50,20 @@ import org.vividus.ui.web.action.search.WebLocatorType;
 @ExtendWith(MockitoExtension.class)
 class DropdownStepsTests
 {
+    private static final String MULTIPLE = "multiple";
+    private static final String OPTION = "option";
+    private static final String DROPDOWN = "Dropdown";
     private static final String SELECT = "select";
-    private static final String DROP_DOWN_WITH_THE_NAME_DROP_DOWN_LIST_NAME =
-            "Drop down with the name 'dropDownListName'";
-    private static final String SELECTED_OPTION_IN_DROP_DOWN = "Selected option in drop down";
+    private static final String SELECTED_OPTION_IN_DROP_DOWN = "Selected option in dropdown";
     private static final String ITEM_VALUE = "1";
-    private static final String SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN = "Selected options are present in drop down";
-    private static final String DROP_DOWN_LIST_NAME = "dropDownListName";
-    private static final String DROP_DOWN_XPATH = ".//select[@*[normalize-space()=\"dropDownListName\"]]";
+    private static final String SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN = "Selected options are present in dropdown";
+    private static final Locator LOCATOR = new Locator(WebLocatorType.XPATH, "//xpath");
     private static final String TEXT = "text";
     private static final String DROPDOWN_EXAMPLES_TABLE = "|state|item|\n|true|text| ";
     private static final String DROPDOWN_SIZE_ASSERTION_MESSAGE =
             "Expected dropdown is of the same size as actual dropdown: ";
-    private static final String TEXT_OF_ITEM_MESSAGE = "Text of actual item at position [%s]";
-    private static final String STATE_OF_ITEM_MESSAGE = "State of actual item at position [%s]";
-    private static final String A_DROP_DOWN = "A drop down";
-    private static final String XPATH = "//xpath";
+    private static final String TEXT_OF_ITEM_MESSAGE = "Text of actual option at position [%s]";
+    private static final String STATE_OF_ITEM_MESSAGE = "State of actual option at position [%s]";
 
     @Mock
     private IWebElementActions webElementActions;
@@ -88,82 +83,49 @@ class DropdownStepsTests
     @Mock
     private IFieldActions fieldActions;
 
-    @Test
-    void ifDropDownWithNameFound()
+    @ParameterizedTest
+    @ValueSource(strings = { TEXT, TEXT + " "})
+    void shouldVerifySelectWithSelectedOptionExists(String text)
     {
-        dropdownSteps.isDropDownWithNameFound(DROP_DOWN_LIST_NAME);
-        verify(baseValidations).assertIfElementExists(DROP_DOWN_WITH_THE_NAME_DROP_DOWN_LIST_NAME,
-                new Locator(WebLocatorType.XPATH, DROP_DOWN_XPATH));
-    }
-
-    @Test
-    void ifDropDownWithNameExists()
-    {
-        mockDropDownWithName(TEXT);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
+        when(webElement.getTagName()).thenReturn(SELECT);
+        List<WebElement> options = List.of(mock(WebElement.class));
+        when(softAssert.assertTrue(SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN, true)).thenReturn(Boolean.TRUE);
+        WebElement firstSelectedOption = options.get(0);
+        when(webElement.findElements(By.tagName(OPTION))).thenReturn(singletonList(firstSelectedOption));
+        when(firstSelectedOption.isSelected()).thenReturn(true);
+        when(firstSelectedOption.getText()).thenReturn(text);
+        dropdownSteps.doesDropdownHaveFirstSelectedOption(LOCATOR, TEXT);
         verify(softAssert).assertEquals(SELECTED_OPTION_IN_DROP_DOWN, TEXT, TEXT);
     }
 
     @Test
-    void ifDropDownWithNameExistsSpaces()
+    void shouldFailIfNoOptionsSelected()
     {
-        mockDropDownWithName(TEXT + " ");
-        verify(softAssert).assertEquals(SELECTED_OPTION_IN_DROP_DOWN, TEXT, TEXT);
-    }
-
-    @Test
-    void ifDropDownWithNameExistsSelectNull()
-    {
-        Select dropDown = mock(Select.class);
-        DropdownSteps spy = Mockito.spy(dropdownSteps);
-        Mockito.lenient().doReturn(dropDown).when(spy).isDropDownWithNameFound(null);
-        spy.ifDropDownWithNameExists(DROP_DOWN_LIST_NAME, TEXT);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
+        when(webElement.getTagName()).thenReturn(SELECT);
+        when(softAssert.assertTrue(SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN, false)).thenReturn(Boolean.FALSE);
+        when(webElement.findElements(By.tagName(OPTION))).thenReturn(List.of());
+        dropdownSteps.doesDropdownHaveFirstSelectedOption(LOCATOR, TEXT);
         verify(softAssert, never()).assertEquals(SELECTED_OPTION_IN_DROP_DOWN, TEXT, TEXT);
     }
 
     @Test
-    void ifDropDownWithNameExistsSelectedOptionsAreNotPresent()
+    void shouldFailIfNoSelectFound()
     {
-        Select dropDown = mock(Select.class);
-        DropdownSteps spy = Mockito.spy(dropdownSteps);
-        doReturn(dropDown).when(spy).isDropDownWithNameFound(DROP_DOWN_LIST_NAME);
-        when(dropDown.getAllSelectedOptions()).thenReturn(List.of());
-        Mockito.lenient().when(softAssert.assertTrue(SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN, true))
-                .thenReturn(Boolean.FALSE);
-        spy.ifDropDownWithNameExists(DROP_DOWN_LIST_NAME, TEXT);
-        verify(softAssert, never()).assertEquals(SELECTED_OPTION_IN_DROP_DOWN, TEXT, TEXT);
-    }
-
-    @Test
-    void ifDropDownWithNameFoundState()
-    {
-        WebElement element = findDropDownListWithParameters(false);
-        dropdownSteps.isDropDownWithNameFound(DropDownState.ENABLED, DROP_DOWN_LIST_NAME);
-        verify(baseValidations).assertElementState(eq("The found drop down is ENABLED"), eq(DropDownState.ENABLED),
-                argThat((WrapsElement select) -> select.getWrappedElement().equals(element)));
-    }
-
-    @Test
-    void testDoesNotDropDownExist()
-    {
-        dropdownSteps.doesNotDropDownExist(DROP_DOWN_LIST_NAME);
-        verify(baseValidations).assertIfElementDoesNotExist(DROP_DOWN_WITH_THE_NAME_DROP_DOWN_LIST_NAME,
-                new Locator(WebLocatorType.XPATH, new SearchParameters(DROP_DOWN_XPATH, Visibility.ALL)));
-    }
-
-    @Test
-    void testSelectItemInDDLSelectPresentOptionExist()
-    {
-        WebElement element = findDropDownListWithParameters(false);
-        dropdownSteps.selectItemInDDL(TEXT, DROP_DOWN_LIST_NAME);
-        verify(fieldActions).selectItemInDropDownList(
-                argThat((Select select) -> select.getWrappedElement().equals(element)), eq(TEXT), eq(false));
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(null);
+        dropdownSteps.doesDropdownHaveFirstSelectedOption(LOCATOR, TEXT);
+        verifyNoInteractions(softAssert);
     }
 
     @Test
     void testSelectItemInDDLSingleSelectAdditable()
     {
-        WebElement element = findDropDownListWithParameters(false);
-        dropdownSteps.addItemInDDL(TEXT, DROP_DOWN_LIST_NAME);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
+        when(webElement.getTagName()).thenReturn(SELECT);
+        when(webElement.getAttribute(MULTIPLE)).thenReturn(Boolean.toString(false));
+        WebElement element = webElement;
+        dropdownSteps.addOptionInDropdown(TEXT, LOCATOR);
         verify(fieldActions).selectItemInDropDownList(
                 argThat((Select select) -> select.getWrappedElement().equals(element)), eq(TEXT), eq(true));
     }
@@ -172,12 +134,15 @@ class DropdownStepsTests
     void testDoesDropDownListContainItems()
     {
         ExamplesTable dropDownItems = new ExamplesTable(DROPDOWN_EXAMPLES_TABLE);
-        WebElement element = findDropDownListWithParameters(true);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
+        when(webElement.getTagName()).thenReturn(SELECT);
+        when(webElement.getAttribute(MULTIPLE)).thenReturn(Boolean.toString(true));
+        WebElement element = webElement;
         addOptionsToSelect(element, TEXT);
         when(softAssert.assertEquals(DROPDOWN_SIZE_ASSERTION_MESSAGE,
                 dropDownItems.getRowsAsParameters(true).size(), 1)).thenReturn(true);
         when(webElement.isSelected()).thenReturn(true);
-        dropdownSteps.doesDropDownListContainItems(DROP_DOWN_LIST_NAME, dropDownItems);
+        dropdownSteps.doesDropdownContainOptions(LOCATOR, dropDownItems);
         verify(softAssert).assertEquals(String.format(TEXT_OF_ITEM_MESSAGE, ITEM_VALUE), TEXT,
                 TEXT);
         verify(softAssert).assertEquals(String.format(STATE_OF_ITEM_MESSAGE, ITEM_VALUE), true,
@@ -188,7 +153,7 @@ class DropdownStepsTests
     void testDoesDropDownListContainItemsNullDropDown()
     {
         ExamplesTable dropDownItems = new ExamplesTable(DROPDOWN_EXAMPLES_TABLE);
-        dropdownSteps.doesDropDownListContainItems(DROP_DOWN_LIST_NAME, dropDownItems);
+        dropdownSteps.doesDropdownContainOptions(LOCATOR, dropDownItems);
         verify(softAssert, never()).assertEquals(String.format(TEXT_OF_ITEM_MESSAGE, ITEM_VALUE),
                 TEXT, TEXT);
         verify(softAssert, never())
@@ -199,11 +164,14 @@ class DropdownStepsTests
     void testDoesDropDownListContainItemsListIsEmpty()
     {
         ExamplesTable dropDownItems = new ExamplesTable(DROPDOWN_EXAMPLES_TABLE);
-        WebElement element = findDropDownListWithParameters(true);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
+        when(webElement.getTagName()).thenReturn(SELECT);
+        when(webElement.getAttribute(MULTIPLE)).thenReturn(Boolean.toString(true));
+        WebElement element = webElement;
         addOptionsToSelect(element, "itemValue");
         when(softAssert.assertEquals(DROPDOWN_SIZE_ASSERTION_MESSAGE,
                 dropDownItems.getRowsAsParameters(true).size(), 1)).thenReturn(false);
-        dropdownSteps.doesDropDownListContainItems(DROP_DOWN_LIST_NAME, dropDownItems);
+        dropdownSteps.doesDropdownContainOptions(LOCATOR, dropDownItems);
         verify(softAssert, never()).assertEquals(String.format(TEXT_OF_ITEM_MESSAGE, ITEM_VALUE),
                 TEXT, TEXT);
         verify(softAssert, never())
@@ -213,51 +181,22 @@ class DropdownStepsTests
     @Test
     void testSelectTextFromDropDownByLocatorXpath()
     {
-        Locator locator = new Locator(WebLocatorType.XPATH, XPATH);
-        when(baseValidations.assertIfElementExists(A_DROP_DOWN, locator)).thenReturn(webElement);
+        when(baseValidations.assertIfElementExists(DROPDOWN, LOCATOR)).thenReturn(webElement);
         when(webElement.getTagName()).thenReturn(SELECT);
-        dropdownSteps.selectTextFromDropDownByLocator(TEXT, locator);
+        dropdownSteps.selectOptionInDropdown(TEXT, LOCATOR);
         verify(fieldActions).selectItemInDropDownList(any(Select.class), eq(TEXT), eq(false));
     }
 
     @Test
     void testSelectTextFromDropDownByLocatorXpathNoElement()
     {
-        Locator locator = new Locator(WebLocatorType.XPATH, XPATH);
-        when(baseValidations.assertIfElementExists(A_DROP_DOWN, locator)).thenReturn(null);
-        dropdownSteps.selectTextFromDropDownByLocator(TEXT, locator);
+        dropdownSteps.selectOptionInDropdown(TEXT, LOCATOR);
         verifyNoInteractions(fieldActions);
-    }
-
-    private WebElement findDropDownListWithParameters(boolean isMultiple)
-    {
-        when(baseValidations.assertIfElementExists(DROP_DOWN_WITH_THE_NAME_DROP_DOWN_LIST_NAME,
-                new Locator(WebLocatorType.XPATH, DROP_DOWN_XPATH))).thenReturn(webElement);
-        when(webElement.getTagName()).thenReturn(SELECT);
-        when(webElement.getAttribute("multiple")).thenReturn(Boolean.toString(isMultiple));
-        return webElement;
     }
 
     private void addOptionsToSelect(WebElement element, String selectText)
     {
-        when(element.findElements(By.tagName("option"))).thenReturn(singletonList(webElement));
+        when(element.findElements(By.tagName(OPTION))).thenReturn(singletonList(webElement));
         Mockito.lenient().when(webElementActions.getElementText(webElement)).thenReturn(selectText);
-    }
-
-    private void mockDropDownWithName(String firstOptionText)
-    {
-        Select dropDown = mock(Select.class);
-        DropdownSteps spy = Mockito.spy(dropdownSteps);
-        doReturn(dropDown).when(spy).isDropDownWithNameFound(DROP_DOWN_LIST_NAME);
-
-        List<WebElement> options = List.of(mock(WebElement.class));
-        when(dropDown.getAllSelectedOptions()).thenReturn(options);
-        when(softAssert.assertTrue(SELECTED_OPTIONS_ARE_PRESENT_IN_DROP_DOWN, true))
-                .thenReturn(Boolean.TRUE);
-
-        WebElement firstSelectedOption = options.get(0);
-        when(dropDown.getFirstSelectedOption()).thenReturn(firstSelectedOption);
-        when(firstSelectedOption.getText()).thenReturn(firstOptionText);
-        spy.ifDropDownWithNameExists(DROP_DOWN_LIST_NAME, TEXT);
     }
 }
