ab7e44828f105a0aa222266bb81cb762cd2cfb7a
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-10-02 16:34:00 +0300

    revert false positive test

--- vividus-plugin-saucelabs/src/test/java/org/vividus/selenium/sauce/SauceConnectOptionsTests.java
+++ vividus-plugin-saucelabs/src/test/java/org/vividus/selenium/sauce/SauceConnectOptionsTests.java
@@ -16,8 +16,9 @@
 
 package org.vividus.selenium.sauce;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -35,7 +36,6 @@ import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.vividus.selenium.sauce.SauceConnectOptions;
 
 @RunWith(PowerMockRunner.class)
 public class SauceConnectOptionsTests
@@ -218,7 +218,7 @@ public class SauceConnectOptionsTests
     @Test
     public void testNotEqualsToNull()
     {
-        assertNotEquals(null, createDefaultOptions());
+        assertFalse(createDefaultOptions().equals(null));
     }
 
     @Test
