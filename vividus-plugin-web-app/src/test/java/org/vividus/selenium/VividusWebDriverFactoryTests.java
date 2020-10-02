48850982ad772ea7912cb2030de806a3ff10b0a8
Ivan Kalinin <ivan_kalinin1@epam.com>
2020-10-02 13:22:02 +0300

    Add VividusWebDriverFactory.java tests

--- vividus-plugin-web-app/src/test/java/org/vividus/selenium/VividusWebDriverFactoryTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/selenium/VividusWebDriverFactoryTests.java
@@ -22,32 +22,25 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.net.InetAddress;
 import java.util.List;
 import java.util.Optional;
 import java.util.Properties;
 
-import com.browserup.bup.client.ClientUtil;
-
 import org.jbehave.core.model.Meta;
 import org.jbehave.core.model.Scenario;
 import org.jbehave.core.model.Story;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.openqa.selenium.Capabilities;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Options;
-import org.openqa.selenium.WebDriver.Window;
+import org.openqa.selenium.WrapsDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.events.WebDriverEventListener;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 import org.vividus.bdd.context.IBddRunContext;
 import org.vividus.bdd.model.RunningScenario;
@@ -57,9 +50,8 @@ import org.vividus.selenium.manager.IWebDriverManager;
 import org.vividus.selenium.manager.IWebDriverManagerContext;
 import org.vividus.selenium.manager.WebDriverManagerParameter;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ ClientUtil.class, InetAddress.class, VividusWebDriverFactory.class })
-public class VividusWebDriverFactoryTests
+@ExtendWith(MockitoExtension.class)
+class VividusWebDriverFactoryTests
 {
     private static final String FIREFOX = "firefox";
     private static final String TEST = "Test";
@@ -77,7 +69,7 @@ public class VividusWebDriverFactoryTests
     @Mock(extraInterfaces = HasCapabilities.class)
     private WebDriver driver;
 
-    @Before
+    @BeforeEach
     public void beforeEach()
     {
         vividusWebDriverFactory = new VividusWebDriverFactory(true, webDriverManagerContext, bddRunContext,
@@ -91,22 +83,17 @@ public class VividusWebDriverFactoryTests
 
     private void runCreateTest(boolean remoteExecution, String browserName, RunningStory runningStory) throws Exception
     {
-        vividusWebDriverFactory.setWebDriverEventListeners(List.of(webDriverEventListener));
+        List<WebDriverEventListener> eventListeners = List.of(webDriverEventListener);
+        vividusWebDriverFactory.setWebDriverEventListeners(eventListeners);
 
         when(bddRunContext.getRunningStory()).thenReturn(runningStory);
-        EventFiringWebDriver eventFiringWebDriver = mock(EventFiringWebDriver.class);
-        PowerMockito.whenNew(EventFiringWebDriver.class).withArguments(driver).thenReturn(eventFiringWebDriver);
-        Options options = mock(Options.class);
-        when(eventFiringWebDriver.manage()).thenReturn(options);
-        Window window = mock(Window.class);
-        when(options.window()).thenReturn(window);
-        when(eventFiringWebDriver.getCapabilities()).thenReturn(mock(Capabilities.class));
         BrowserWindowSize windowSize = new BrowserWindowSize("1920x1080");
         when(browserWindowSizeProvider.getBrowserWindowSize(remoteExecution))
                 .thenReturn(windowSize);
         VividusWebDriver vividusWebDriver = vividusWebDriverFactory.create();
-        assertEquals(eventFiringWebDriver, vividusWebDriver.getWrappedDriver());
-        verify(eventFiringWebDriver).register(webDriverEventListener);
+        WebDriver eventFiringDriver = vividusWebDriver.getWrappedDriver();
+        assertEquals(driver, ((WrapsDriver) eventFiringDriver).getWrappedDriver());
+        assertEquals(eventListeners, Whitebox.getInternalState(eventFiringDriver, "eventListeners"));
         assertEquals(remoteExecution, vividusWebDriver.isRemote());
         verify(webDriverManagerContext).reset(WebDriverManagerParameter.DESIRED_CAPABILITIES);
         verify(webDriverManager).resize(driver, windowSize);
@@ -134,21 +121,21 @@ public class VividusWebDriverFactoryTests
     }
 
     @Test
-    public void testCreateWebDriverRemoteWithProxy() throws Exception
+    void testCreateWebDriverRemoteWithProxy() throws Exception
     {
         when(webDriverFactory.getRemoteWebDriver(any(DesiredCapabilities.class))).thenReturn(driver);
         runCreateTest(true, null);
     }
 
     @Test
-    public void testCreateWebDriverRemoteNoProxy() throws Exception
+    void testCreateWebDriverRemoteNoProxy() throws Exception
     {
         when(webDriverFactory.getRemoteWebDriver(any(DesiredCapabilities.class))).thenReturn(driver);
         runCreateTest(true, null);
     }
 
     @Test
-    public void testCreateWebDriverNoRunningScenario() throws Exception
+    void testCreateWebDriverNoRunningScenario() throws Exception
     {
         when(webDriverFactory.getRemoteWebDriver(any(DesiredCapabilities.class))).thenReturn(driver);
         RunningStory runningStory = new RunningStory();
@@ -157,7 +144,7 @@ public class VividusWebDriverFactoryTests
     }
 
     @Test
-    public void testCreateWebDriverLocalWithProxy() throws Exception
+    void testCreateWebDriverLocalWithProxy() throws Exception
     {
         notRemoteExecution();
         when(webDriverFactory.getWebDriver(any(DesiredCapabilities.class))).thenReturn(driver);
@@ -170,10 +157,22 @@ public class VividusWebDriverFactoryTests
     }
 
     @Test
-    public void testCreateWebDriverLocalNoProxy() throws Exception
+    void testCreateWebDriverLocalNoProxy() throws Exception
     {
         notRemoteExecution();
         when(webDriverFactory.getWebDriver(any(DesiredCapabilities.class))).thenReturn(driver);
         runCreateTest(false, FIREFOX);
+    }
+
+    @Test
+    void shouldAddProxyCapabilitiesWhenProxyStarted()
+    {
+        DesiredCapabilities desiredCapabilities = mock(DesiredCapabilities.class);
+        when(proxy.isStarted()).thenReturn(true);
+        Proxy seleniumProxy = mock(Proxy.class);
+        when(proxy.createSeleniumProxy()).thenReturn(seleniumProxy);
+        vividusWebDriverFactory.setDesiredCapabilities(desiredCapabilities);
+        verify(desiredCapabilities).setCapability(CapabilityType.PROXY, seleniumProxy);
+        verify(desiredCapabilities).setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
     }
 }
