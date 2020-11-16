959ce0f16c216fc27d343b4d0a89a0808d6f3899
Valery Yatsynovich <valery_yatsynovich@epam.com>
2020-11-16 11:09:42 +0300

    [http-client] Remove unused methods from ClientBuilderUtils (#1142)

--- vividus-http-client/src/test/java/org/vividus/http/client/ClientBuilderUtilsTests.java
+++ vividus-http-client/src/test/java/org/vividus/http/client/ClientBuilderUtilsTests.java
@@ -19,67 +19,21 @@ package org.vividus.http.client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mockConstruction;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.cookie.Cookie;
-import org.apache.http.impl.cookie.BasicClientCookie;
-import org.apache.http.message.BasicNameValuePair;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedConstruction;
 
 class ClientBuilderUtilsTests
 {
-    private static final AuthScope DEFAULT_AUTH_SCOPE = AuthScope.ANY;
-
-    @Test
-    void testCreateUrlEncodedFormEntity() throws IOException
-    {
-        List<NameValuePair> params = new ArrayList<>();
-        params.add(new BasicNameValuePair("param1", "value1"));
-        params.add(new BasicNameValuePair("param2", "value2"));
-        UrlEncodedFormEntity expectedEntity = new UrlEncodedFormEntity(params);
-        UrlEncodedFormEntity actualEntity = ClientBuilderUtils.createUrlEncodedFormEntity(params);
-        assertEquals(expectedEntity.getContentLength(), actualEntity.getContentLength());
-    }
-
-    @Test
-    void testCreateUrlEncodedFormEntityThrowingException() throws IOException
-    {
-        UrlEncodedFormEntity expectedEntity = new UrlEncodedFormEntity(new ArrayList<>());
-        UrlEncodedFormEntity actualEntity = ClientBuilderUtils.createUrlEncodedFormEntity(new ArrayList<>());
-        assertEquals(expectedEntity.getContentLength(), actualEntity.getContentLength());
-    }
-
-    @Test
-    void testCreateCredentialsProviderSplit()
-    {
-        CredentialsProvider actualProvider = ClientBuilderUtils.createCredentialsProvider("user:pass");
-        verifyProvider(actualProvider, DEFAULT_AUTH_SCOPE, "user", "pass");
-    }
-
     @Test
     void testCreateCredentialsProviderSplitWithScope()
     {
         final AuthScope authScope = new AuthScope("host1", 1);
         CredentialsProvider actualProvider = ClientBuilderUtils.createCredentialsProvider(authScope, "user13:pass13");
         verifyProvider(actualProvider, authScope, "user13", "pass13");
-    }
-
-    @Test
-    void testCreateCredentialsProviderSimple()
-    {
-        String user = "user2";
-        String pass = "pass123";
-        CredentialsProvider actualProvider = ClientBuilderUtils.createCredentialsProvider(user, pass);
-        verifyProvider(actualProvider, DEFAULT_AUTH_SCOPE, user, pass);
     }
 
     @Test
@@ -90,15 +44,6 @@ class ClientBuilderUtilsTests
         AuthScope authScope = new AuthScope("host2", 1);
         CredentialsProvider actualProvider = ClientBuilderUtils.createCredentialsProvider(authScope, user, pass);
         verifyProvider(actualProvider, authScope, user, pass);
-    }
-
-    @Test
-    void testCreateCookieStore()
-    {
-        Cookie cookie = new BasicClientCookie("name", "value");
-        List<Cookie> result = ClientBuilderUtils.createCookieStore(Collections.singleton(cookie)).getCookies();
-        Assertions.assertFalse(result.isEmpty());
-        assertEquals(result.get(0), cookie);
     }
 
     @Test
