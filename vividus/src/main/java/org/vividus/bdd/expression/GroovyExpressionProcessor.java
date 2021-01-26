c7045d3237c4670519a7cf39e48462c172afbd85
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-01-26 17:28:36 +0300

    [vividus] Fix typo: GroovyExpressionProcessor

--- vividus/src/main/java/org/vividus/bdd/expression/GroovyExpressionProcessor.java
+++ vividus/src/main/java/org/vividus/bdd/expression/GroovyExpressionProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,9 +22,9 @@ import org.springframework.scripting.groovy.GroovyScriptEvaluator;
 import org.springframework.scripting.support.StaticScriptSource;
 
 @Named
-public class GroovyEpxpressionProcessor extends UnaryExpressionProcessor
+public class GroovyExpressionProcessor extends UnaryExpressionProcessor
 {
-    public GroovyEpxpressionProcessor(GroovyScriptEvaluator groovyScriptEvaluator)
+    public GroovyExpressionProcessor(GroovyScriptEvaluator groovyScriptEvaluator)
     {
         super("evalGroovy", input -> String.valueOf(
                 groovyScriptEvaluator.evaluate(new StaticScriptSource(input))));
--- vividus/src/test/java/org/vividus/bdd/expression/GroovyExpressionProcessorTests.java
+++ vividus/src/test/java/org/vividus/bdd/expression/GroovyExpressionProcessorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,13 +31,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.scripting.groovy.GroovyScriptEvaluator;
 
 @ExtendWith(MockitoExtension.class)
-class GroovyEpxpressionProcessorTests
+class GroovyExpressionProcessorTests
 {
     @Mock
     private GroovyScriptEvaluator evaulator;
 
     @InjectMocks
-    private GroovyEpxpressionProcessor processor;
+    private GroovyExpressionProcessor processor;
 
     @Test
     void shouldResolveGroovyExpressions()
