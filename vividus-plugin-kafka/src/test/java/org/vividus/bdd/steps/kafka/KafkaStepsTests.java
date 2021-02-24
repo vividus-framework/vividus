b94506c0e14156aefe5592aefca3b53e01e367a0
Uladzislau Arlouski <uarlouski@gmail.com>
2021-02-24 15:22:29 +0300

    [plugin-kafka] Ensure only one kafka listener exists per thread (#1456)

--- vividus-plugin-kafka/src/test/java/org/vividus/bdd/steps/kafka/KafkaStepsTests.java
+++ vividus-plugin-kafka/src/test/java/org/vividus/bdd/steps/kafka/KafkaStepsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.vividus.bdd.steps.kafka;
 
 import static com.github.valfirst.slf4jtest.LoggingEvent.info;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -25,9 +26,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.github.valfirst.slf4jtest.TestLogger;
 import com.github.valfirst.slf4jtest.TestLoggerFactory;
@@ -37,8 +40,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.kafka.listener.GenericMessageListenerContainer;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.vividus.bdd.context.IBddVariableContext;
 import org.vividus.softassert.SoftAssert;
 import org.vividus.testcontext.TestContext;
@@ -47,6 +53,8 @@ import org.vividus.util.property.IPropertyParser;
 @ExtendWith({ MockitoExtension.class, TestLoggerFactoryExtension.class })
 class KafkaStepsTests
 {
+    private static final String LISTENER_IS_STOPPED = "Kafka message listener is stopped";
+
     private final TestLogger logger = TestLoggerFactory.getTestLogger(KafkaSteps.class);
 
     @Mock private IPropertyParser propertyParser;
@@ -86,7 +94,7 @@ class KafkaStepsTests
         kafkaSteps.cleanUp();
         verify(container).stop();
         verify(testContext).remove(key);
-        assertThat(logger.getLoggingEvents(), is(List.of(info("Kafka message listener is stopped"))));
+        assertThat(logger.getLoggingEvents(), is(List.of(info(LISTENER_IS_STOPPED))));
     }
 
     @Test
@@ -96,5 +104,35 @@ class KafkaStepsTests
         kafkaSteps.cleanUp();
         verifyNoMoreInteractions(testContext);
         assertThat(logger.getLoggingEvents(), is(List.of()));
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Test
+    void shouldStopStartedKafkaListenerIfNewKafkaListenerIsCreated()
+    {
+        try (MockedConstruction<KafkaMessageListenerContainer> construction = Mockito.mockConstruction(
+                KafkaMessageListenerContainer.class,
+                withSettings().extraInterfaces(GenericMessageListenerContainer.class)))
+        {
+            String topic = "topic";
+            kafkaSteps.startKafkaListener(Set.of(topic));
+            KafkaMessageListenerContainer container = construction.constructed().get(0);
+
+            when(testContext.get(GenericMessageListenerContainer.class)).thenReturn(container);
+
+            kafkaSteps.startKafkaListener(Set.of(topic));
+
+            verify(testContext).put(GenericMessageListenerContainer.class, container);
+            verify(testContext).put(GenericMessageListenerContainer.class, construction.constructed().get(1));
+            assertThat(construction.constructed(), hasSize(2));
+
+            verify(testContext).remove(GenericMessageListenerContainer.class);
+
+            String listenerIsStarted = "Kafka message listener is started";
+            assertThat(logger.getLoggingEvents(),
+                    is(List.of(info(listenerIsStarted),
+                               info(LISTENER_IS_STOPPED),
+                               info(listenerIsStarted))));
+        }
     }
 }
