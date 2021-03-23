5e2624212cf730e1848e67c4a396b3dc7cfe7b0b
Uladzislau Arlouski <uarlouski@gmail.com>
2021-03-23 17:01:14 +0300

    [extension-selenium] Add support for tvOS remote execution

--- vividus-extension-selenium/src/test/java/org/vividus/selenium/manager/GenericWebDriverManagerTests.java
+++ vividus-extension-selenium/src/test/java/org/vividus/selenium/manager/GenericWebDriverManagerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -235,6 +235,16 @@ class GenericWebDriverManagerTests
         Map<String, Object> capabilities = Map.of(
                 CapabilityType.PLATFORM_NAME, Platform.MAC.toString(),
                 BROWSER_TYPE, BrowserType.IPAD
+        );
+        mockWebDriverHavingCapabilities(capabilities);
+        assertTrue(driverManager.isIOS());
+    }
+
+    @Test
+    void testIsTvOS()
+    {
+        Map<String, Object> capabilities = Map.of(
+                CapabilityType.PLATFORM_NAME, MobilePlatform.TVOS
         );
         mockWebDriverHavingCapabilities(capabilities);
         assertTrue(driverManager.isIOS());
