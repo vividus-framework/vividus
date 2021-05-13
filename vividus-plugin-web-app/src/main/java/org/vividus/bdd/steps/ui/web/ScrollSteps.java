19b339e944438575159831b2e42896fa1c4374ce
Ivan Kalinin <ivan_kalinin1@epam.com>
2021-05-13 10:46:08 +0300

    [plugin-web-app] Fix typo in javadocs

--- vividus-plugin-web-app/src/main/java/org/vividus/bdd/steps/ui/web/ScrollSteps.java
+++ vividus-plugin-web-app/src/main/java/org/vividus/bdd/steps/ui/web/ScrollSteps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ public class ScrollSteps
      * <br><b>LEFT</b> - start of a page/element horizontally,
      * <br><b>RIGHT</b> - end of a page/element horizontally,
      * <br><b>TOP</b> - start of a page/element vertically,
-     * <br><b>BOTTOM</b> - end of a page/element horizontally
+     * <br><b>BOTTOM</b> - end of a page/element vertically
      */
     @When("I scroll context to $scrollDirection edge")
     public void scrollContextIn(ScrollDirection scrollDirection)
