d25a6a7d9b8b42e1c7510617913d27a59e896cda
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-08-26 14:33:52 +0300

    [plugin-email] Simplify content type detection (#1902)

--- vividus-plugin-email/src/main/java/org/vividus/bdd/email/factory/EmailMessageFactory.java
+++ vividus-plugin-email/src/main/java/org/vividus/bdd/email/factory/EmailMessageFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,7 +27,7 @@ import javax.mail.MessagingException;
 import javax.mail.Multipart;
 import javax.mail.Part;
 
-import org.apache.tika.mime.MediaType;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vividus.bdd.email.model.EmailMessage;
@@ -88,8 +88,8 @@ public class EmailMessageFactory
                 continue;
             }
             String contentType = part.getContentType();
-            MediaType mediaType = MediaType.parse(part.getContentType());
-            if (!"text".equals(mediaType.getType()))
+            String type = StringUtils.substringBefore(contentType, '/');
+            if (!"text".equalsIgnoreCase(type))
             {
                 LOGGER.info("Skip saving of content with the content type '{}'", contentType);
                 continue;
