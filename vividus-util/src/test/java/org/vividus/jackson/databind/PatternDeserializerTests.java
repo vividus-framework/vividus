af25966ab23f5b4bf39ebae102ea137482952fe7
Ivan Kalinin <ivan_kalinin1@epam.com>
2021-09-03 17:35:01 +0300

    [util] Fix PMD violations

--- vividus-util/src/test/java/org/vividus/jackson/databind/PatternDeserializerTests.java
+++ vividus-util/src/test/java/org/vividus/jackson/databind/PatternDeserializerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -27,6 +27,7 @@ import com.fasterxml.jackson.core.JsonParser;
 
 import org.junit.jupiter.api.Test;
 
+@SuppressWarnings("PMD.CloseResource")
 class PatternDeserializerTests
 {
     @Test
