2e0847e8189e44f0e1b704b9042e0af07c2cffe8
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-10-05 17:49:21 +0300

    Deprecate `dymanicPatterns` in known issues (#2050)

--- vividus-soft-assert/src/main/java/org/vividus/softassert/issue/IKnownIssueDataProvider.java
+++ vividus-soft-assert/src/main/java/org/vividus/softassert/issue/IKnownIssueDataProvider.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,6 +18,7 @@ package org.vividus.softassert.issue;
 
 import java.util.Optional;
 
+@Deprecated(since = "0.3.10", forRemoval = true)
 public interface IKnownIssueDataProvider
 {
     Optional<String> getData();
