845c54abc1348c606fd891eed3486e6fa01e97bf
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-10-29 00:13:32 +0300

    [bdd-engine] Normalize resources paths to unify added/removed items

--- vividus-bdd-engine/src/main/java/org/vividus/bdd/PathFinder.java
+++ vividus-bdd-engine/src/main/java/org/vividus/bdd/PathFinder.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,9 +30,6 @@ import org.vividus.bdd.resource.IBddResourceLoader;
 
 public class PathFinder implements IPathFinder
 {
-    private static final String PLUS_SIGN_REGEX = "\\+";
-    private static final String PLUS_SIGN_ENCODED = "%2b";
-
     private IBddResourceLoader bddResourceLoader;
 
     @Override
@@ -54,7 +51,7 @@ public class PathFinder implements IPathFinder
                     resourcePattern);
             for (Resource resource : foundResources)
             {
-                String resourceUri = resource.getURI().toASCIIString().replaceAll(PLUS_SIGN_REGEX, PLUS_SIGN_ENCODED);
+                String resourceUri = resource.getURI().normalize().toASCIIString().replace("+", "%2b");
                 consumer.accept(URLDecoder.decode(resourceUri, StandardCharsets.UTF_8));
             }
         }
