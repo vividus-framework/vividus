0a0e77065d2344b86f90fb0fb1ce58b428ec95f2
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-11-01 17:11:10 +0300

    [extension-selenium] Deprecate non-generic meta tags setting capabilities

--- vividus-extension-selenium/src/main/java/org/vividus/selenium/SauceLabsCapabilityType.java
+++ vividus-extension-selenium/src/main/java/org/vividus/selenium/SauceLabsCapabilityType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,10 +18,6 @@ package org.vividus.selenium;
 
 public interface SauceLabsCapabilityType
 {
-    String SCREEN_RESOLUTION = "screen-resolution";
-    String DEVICE_ORIENTATION = "device-orientation";
-    String IEDRIVER_VERSION = "iedriver-version";
     String DEVICE = "device";
     String CAPABILITIES = "capabilities";
-    String SELENIUM_VERSION = "seleniumVersion";
 }
