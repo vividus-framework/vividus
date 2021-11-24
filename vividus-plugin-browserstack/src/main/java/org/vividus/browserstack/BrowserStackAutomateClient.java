c22ebe70afa438ec0dc4b40729fcbf925eff9fcf
Valery Yatsynovich <valery_yatsynovich@epam.com>
2021-11-24 16:53:39 +0300

    [plugin-browserstack] Increase network logs wait timeout from 60 to 90 seconds

--- vividus-plugin-browserstack/src/main/java/org/vividus/browserstack/BrowserStackAutomateClient.java
+++ vividus-plugin-browserstack/src/main/java/org/vividus/browserstack/BrowserStackAutomateClient.java
@@ -35,8 +35,8 @@ import org.vividus.util.wait.Waiter;
 
 public class BrowserStackAutomateClient extends BrowserStackClient
 {
-    private static final int DEFAULT_RETRY = 5;
-    private static final int DEFAULT_SECONDS = 60;
+    private static final int DEFAULT_RETRY = 6;
+    private static final int DEFAULT_SECONDS = 90;
 
     private final Waiter waiter = new DurationBasedWaiter(
             new WaitMode(Duration.ofSeconds(DEFAULT_SECONDS), DEFAULT_RETRY));
