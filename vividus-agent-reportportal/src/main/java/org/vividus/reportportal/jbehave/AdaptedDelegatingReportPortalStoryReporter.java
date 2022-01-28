8121034586328a33ffd300e62d7caa1558874d9a
Ivan Kalinin <ivan_kalinin1@epam.com>
2022-01-28 14:25:08 +0300

    [agent-reportportal] Do not report comments

--- vividus-agent-reportportal/src/main/java/org/vividus/reportportal/jbehave/AdaptedDelegatingReportPortalStoryReporter.java
+++ vividus-agent-reportportal/src/main/java/org/vividus/reportportal/jbehave/AdaptedDelegatingReportPortalStoryReporter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,6 +33,7 @@ import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.jbehave.core.model.Scenario;
 import org.jbehave.core.model.Step;
 import org.jbehave.core.reporters.DelegatingStoryReporter;
+import org.jbehave.core.steps.StepCreator.StepExecutionType;
 import org.jbehave.core.steps.Timing;
 import org.vividus.softassert.event.AssertionFailedEvent;
 
@@ -51,6 +52,10 @@ public class AdaptedDelegatingReportPortalStoryReporter extends DelegatingStoryR
     @Override
     public void beforeStep(Step step)
     {
+        if (step.getExecutionType() == StepExecutionType.COMMENT)
+        {
+            return;
+        }
         runIfNotSystem(step.getStepAsString(), reporter::beforeStep);
     }
 
--- vividus-agent-reportportal/src/test/java/org/vividus/reportportal/jbehave/AdaptedDelegatingReportPortalStoryReporterTests.java
+++ vividus-agent-reportportal/src/test/java/org/vividus/reportportal/jbehave/AdaptedDelegatingReportPortalStoryReporterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -86,6 +86,14 @@ class AdaptedDelegatingReportPortalStoryReporterTests
         String step = "When I play Starfield";
         adaptedReporter.beforeStep(new Step(StepExecutionType.EXECUTABLE, step));
         verify(reporter).beforeStep(step);
+    }
+
+    @Test
+    void shouldNotCallBeforeStepForTheComments()
+    {
+        String step = "!-- 'It just works' T.H.";
+        adaptedReporter.beforeStep(new Step(StepExecutionType.COMMENT, step));
+        verifyNoInteractions(reporter);
     }
 
     @Test
