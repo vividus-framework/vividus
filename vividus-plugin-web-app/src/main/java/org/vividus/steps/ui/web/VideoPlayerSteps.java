3a1dddffec741fcdb7ae2302790dc83c7ab67ef3
Ivan Kalinin <ivan_kalinin1@epam.com>
2022-02-15 13:38:01 +0300

    [plugin-web-app] Allow to use locators in video steps

--- vividus-plugin-web-app/src/main/java/org/vividus/steps/ui/web/VideoPlayerSteps.java
+++ vividus-plugin-web-app/src/main/java/org/vividus/steps/ui/web/VideoPlayerSteps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,64 +16,99 @@
 
 package org.vividus.steps.ui.web;
 
+import java.util.Set;
 import java.util.function.Consumer;
-
-import javax.inject.Inject;
 
 import org.jbehave.core.annotations.When;
 import org.openqa.selenium.WebElement;
+import org.vividus.context.VariableContext;
 import org.vividus.monitor.TakeScreenshotOnFailure;
-import org.vividus.steps.ui.validation.IBaseValidations;
+import org.vividus.steps.ui.validation.BaseValidations;
 import org.vividus.ui.action.search.Locator;
-import org.vividus.ui.web.action.IVideoPlayerActions;
-import org.vividus.ui.web.action.search.WebLocatorType;
-import org.vividus.ui.web.util.LocatorUtil;
+import org.vividus.ui.web.action.VideoPlayerActions;
+import org.vividus.variable.VariableScope;
 
 @TakeScreenshotOnFailure
 public class VideoPlayerSteps
 {
-    @Inject private IBaseValidations baseValidations;
-    @Inject private IVideoPlayerActions videoPlayerActions;
+    private final BaseValidations baseValidations;
+    private final VideoPlayerActions videoPlayerActions;
+    private final VariableContext variableContext;
+
+    public VideoPlayerSteps(BaseValidations baseValidation, VideoPlayerActions videoPlayerActions,
+            VariableContext variableContext)
+    {
+        this.baseValidations = baseValidation;
+        this.videoPlayerActions = videoPlayerActions;
+        this.variableContext = variableContext;
+    }
 
     /**
      * This step rewinds the video in the video player to specified time in seconds. The target element must be
      * &lt;video&gt; tag.
      * @param seconds time in seconds
-     * @param videoPlayerName any attribute value of the element
+     * @param locator Locator to locate element
      */
-    @When("I rewind time to '$number' seconds in the video player with the name '$videoPlayerName'")
-    public void rewindTimeInVideoPlayer(Integer seconds, String videoPlayerName)
+    @When("I rewind time to `$number` seconds in video player located `$locator`")
+    public void rewindTimeInVideoPlayer(Integer seconds, Locator locator)
     {
-        findVideoPlayerAndExecuteAction(videoPlayerName, e -> videoPlayerActions.rewind(e, seconds));
+        findVideoPlayerAndExecuteAction(locator, e -> videoPlayerActions.rewind(e, seconds));
     }
 
     /**
      * This step plays the video in the video player. The target element must be &lt;video&gt; tag.
-     * @param videoPlayerName any attribute value of the element
+     * @param locator Locator to locate element
      */
-    @When("I play video in the video player with the name '$videoPlayerName'")
-    public void playVideoInVideoPlayer(String videoPlayerName)
+    @When("I play video in video player located `$locator`")
+    public void playVideoInVideoPlayer(Locator locator)
     {
-        findVideoPlayerAndExecuteAction(videoPlayerName, videoPlayerActions::play);
+        findVideoPlayerAndExecuteAction(locator, videoPlayerActions::play);
     }
 
     /**
      * This step pauses the video in the video player. The target element must be &lt;video&gt; tag.
-     * @param videoPlayerName any attribute value of the element
+     * @param locator Locator to locate element
      */
-    @When("I pause video in the video player with the name '$videoPlayerName'")
-    public void pauseVideoInVideoPlayer(String videoPlayerName)
+    @When("I pause video in video player located `$locator`")
+    public void pauseVideoInVideoPlayer(Locator locator)
     {
-        findVideoPlayerAndExecuteAction(videoPlayerName, videoPlayerActions::pause);
+        findVideoPlayerAndExecuteAction(locator, videoPlayerActions::pause);
     }
 
-    private void findVideoPlayerAndExecuteAction(String videoPlayerName, Consumer<WebElement> actionConsumer)
+    /**
+     * Saves video player info: <b>duration</b>, <b>currentTime</b>, <b>src</b>, <b>networkState</b>.
+     * For more information about the properties see
+     * <a href="https://www.w3schools.com/tags/ref_av_dom.asp">HTML Audio/Video Properties</a>
+     * @param locator      Locator to locate element
+     * @param scopes       The set (comma separated list of scopes e.g.: STORY, NEXT_BATCHES) of variable's scope<br>
+     *                     <i>Available scopes:</i>
+     *                     <ul>
+     *                       <li><b>STEP</b> - the variable will be available only within the step
+     *                       <li><b>SCENARIO</b> - the variable will be available only within the scenario
+     *                       <li><b>STORY</b> - the variable will be available within the whole story
+     *                       <li><b>NEXT_BATCHES</b> - the variable will be available starting from next batch
+     *                     </ul>
+     * @param variableName The variable name to store results. If the variable name is <b>info</b>, the following
+     *                     variables will be created:
+     *                     <ul>
+     *                       <li><b>${info.src}</b> - The the current source of the audio/video element </li>
+     *                       <li><b>${info.duration}</b> - The length of the current audio/video (in seconds) </li>
+     *                       <li><b>${info.currentTime}</b> - The current playback position in the audio/video
+     *                         (in seconds)</li>
+     *                       <li><b>${info.networkState}</b> - The the current network state of the audio/video.
+     *                         For more information see:
+     *                         <a href="https://www.w3schools.com/tags/av_prop_networkstate.asp">Network State</a></li>
+     *                     </ul>
+     */
+    @When("I save info from video player located `$locator` to $scopes variable `$variableName`")
+    public void saveVideoInfo(Locator locator, Set<VariableScope> scopes, String variableName)
     {
-        WebElement videoPlayer = baseValidations.assertIfElementExists("Video player", new Locator(
-                WebLocatorType.XPATH, LocatorUtil.getXPath("//video[@*='%1$s']", videoPlayerName)));
-        if (videoPlayer != null)
-        {
-            actionConsumer.accept(videoPlayer);
-        }
+        findVideoPlayerAndExecuteAction(locator, v -> variableContext.putVariable(scopes, variableName,
+            videoPlayerActions.getInfo(v)));
+    }
+
+    private void findVideoPlayerAndExecuteAction(Locator locator, Consumer<WebElement> actionConsumer)
+    {
+        baseValidations.assertElementExists("Video player", locator).ifPresent(actionConsumer);
     }
 }
