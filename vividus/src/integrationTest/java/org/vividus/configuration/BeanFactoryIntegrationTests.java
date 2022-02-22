ca4c1916d723f6448a5c20dc3de06792f64b372f
Ivan Kalinin <ivan_kalinin1@epam.com>
2022-02-22 14:09:29 +0300

    [vividus] Resolve SpEL expression in configuration properties (#2510)

--- vividus/src/integrationTest/java/org/vividus/configuration/BeanFactoryIntegrationTests.java
+++ vividus/src/integrationTest/java/org/vividus/configuration/BeanFactoryIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -118,9 +118,10 @@ class BeanFactoryIntegrationTests
 
     @ParameterizedTest
     @CsvSource({
-            "${profile-to-use}, basicenv,               integration",
-            "basicprofile,      ${environments-to-use}, integration",
-            "basicprofile,      basicenv,               ${suite-to-use}"
+            "${profile-to-use},     basicenv,               integration",
+            "basicprofile,          ${environments-to-use}, integration",
+            "basicprofile,          basicenv,               ${suite-to-use}",
+            "${spel-based-profile}, ${spel-based-env},      ${spel-based-suite}"
     })
     void shouldResolvePlaceholdersInConfigurationProperties(String profile, String environments, String suite)
             throws IOException
