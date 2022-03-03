b5c38cb5df5ff5fc549b78e2833a0d7a061f5e5a
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-03-03 14:37:47 +0300

    [plugin-azure-sql-db] Add step listing servers by resource group (#2541)

--- vividus-plugin-azure-sql-db/src/main/java/org/vividus/azure/sql/SqlDatabaseManagementSteps.java
+++ vividus-plugin-azure-sql-db/src/main/java/org/vividus/azure/sql/SqlDatabaseManagementSteps.java
@@ -50,6 +50,33 @@ public class SqlDatabaseManagementSteps
     }
 
     /**
+     * Collects the info about all the SQL Servers under the specified resource group and saves it as JSON to a
+     * variable. For more information, see the <a href=
+     * "https://docs.microsoft.com/en-us/rest/api/sql/2021-08-01-preview/servers/list-by-resource-group">Azure Docs</a>.
+     *
+     * @param resourceGroupName The name of the resource group within the user's subscription to retrieve SQL Servers
+     *                          from. The name is case-insensitive.
+     * @param scopes            The set (comma separated list of scopes e.g.: STORY, NEXT_BATCHES) of the variable
+     *                          scopes.<br>
+     *                          <i>Available scopes:</i>
+     *                          <ul>
+     *                          <li><b>STEP</b> - the variable will be available only within the step,
+     *                          <li><b>SCENARIO</b> - the variable will be available only within the scenario,
+     *                          <li><b>STORY</b> - the variable will be available within the whole story,
+     *                          <li><b>NEXT_BATCHES</b> - the variable will be available starting from next batch
+     *                          </ul>
+     * @param variableName      The variable name to store the info about SQL Servers as JSON.
+     */
+    @When("I collect SQL Servers from resource group `$resourceGroupName` and save them as JSON to $scopes variable "
+            + "`$variableName`")
+    public void listSqlServers(String resourceGroupName, Set<VariableScope> scopes, String variableName)
+    {
+        String urlPath = String.format("subscriptions/%s/resourceGroups/%s/providers/Microsoft.Sql/servers",
+                azureProfile.getSubscriptionId(), resourceGroupName);
+        saveHttpResponseAsVariable(urlPath, scopes, variableName);
+    }
+
+    /**
      * Collects the info about all the databases belonging to the identified SQL Server under the specified resource
      * group and saves it as JSON to a variable. For more information, see the <a
      * href="https://docs.microsoft.com/en-us/rest/api/sql/2021-08-01-preview/databases/list-by-server">Azure Docs</a>.
