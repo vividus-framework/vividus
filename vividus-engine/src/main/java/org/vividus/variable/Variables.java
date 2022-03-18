4e6c9c34b0577459916c0d0541d25e309884a7a1
Ivan Kalinin <ivan_kalinin1@epam.com>
2022-03-18 07:54:00 +0300

    [engine] Fix resolution of default variable value with colon (#2590)

--- vividus-engine/src/main/java/org/vividus/variable/Variables.java
+++ vividus-engine/src/main/java/org/vividus/variable/Variables.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -194,7 +194,7 @@ public class Variables
         {
             this.key = key;
 
-            int colonIndex = key.lastIndexOf(COLON);
+            int colonIndex = key.indexOf(COLON);
             if (colonIndex >= 0)
             {
                 name = key.substring(0, colonIndex);
