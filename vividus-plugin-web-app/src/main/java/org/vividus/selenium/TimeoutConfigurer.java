5333178e7d4773f7e775ca2dd142134c230db311
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-04-06 11:29:04 +0300

    [plugin-web-app] Switch to use non-deprecated API to configire page load timeout

--- vividus-plugin-web-app/src/main/java/org/vividus/selenium/TimeoutConfigurer.java
+++ vividus-plugin-web-app/src/main/java/org/vividus/selenium/TimeoutConfigurer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,7 +42,7 @@ public class TimeoutConfigurer
 
     public void configurePageLoadTimeout(Duration duration, Timeouts timeouts)
     {
-        setTimeout(timeouts, t -> t.pageLoadTimeout(duration.getSeconds(), TimeUnit.SECONDS));
+        setTimeout(timeouts, t -> t.pageLoadTimeout(duration));
     }
 
     private static void setTimeout(Timeouts timeouts, Consumer<Timeouts> consumer)
--- vividus-plugin-web-app/src/test/java/org/vividus/selenium/TimeoutConfigurerTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/selenium/TimeoutConfigurerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -64,7 +64,7 @@ class TimeoutConfigurerTests
     {
         Timeouts timeouts = mock(Timeouts.class);
         timeoutConfigurer.configurePageLoadTimeout(DURATION_PAGE_LOAD_TIMEOUT, timeouts);
-        verify(timeouts).pageLoadTimeout(DURATION_PAGE_LOAD_TIMEOUT.getSeconds(), TimeUnit.SECONDS);
+        verify(timeouts).pageLoadTimeout(DURATION_PAGE_LOAD_TIMEOUT);
     }
 
     @Test
