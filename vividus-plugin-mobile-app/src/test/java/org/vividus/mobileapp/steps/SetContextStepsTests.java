eebb7e1c71c71c3f999a0f13eeeb9057eebe2e7b
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-08-30 16:44:23 +0200

    [plugin-mobile-app] Remove deprecated step switching to web view by index (#3112)

--- vividus-plugin-mobile-app/src/test/java/org/vividus/mobileapp/steps/SetContextStepsTests.java
+++ vividus-plugin-mobile-app/src/test/java/org/vividus/mobileapp/steps/SetContextStepsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package org.vividus.mobileapp.steps;
 
 import static com.github.valfirst.slf4jtest.LoggingEvent.info;
-import static com.github.valfirst.slf4jtest.LoggingEvent.warn;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.is;
@@ -29,15 +28,12 @@ import static org.mockito.Mockito.when;
 import java.util.List;
 import java.util.Set;
 
-import com.github.valfirst.slf4jtest.LoggingEvent;
 import com.github.valfirst.slf4jtest.TestLogger;
 import com.github.valfirst.slf4jtest.TestLoggerFactory;
 import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -52,9 +48,6 @@ class SetContextStepsTests
 {
     private static final String WEB_VIEW_MAIN = "WEBVIEW_MAIN";
     private static final String WEB_VIEWS_FOUND = "Web views found: {}";
-    private static final LoggingEvent DEPRECATION_NOTICE = warn(
-            "This step is deprecated and will be removed in VIVIDUS 0.5.0. The replacement is "
-            + "\"When I switch to web view with name that $comparisonRule `$value`\"");
 
     @Mock private IWebDriverProvider webDriverProvider;
     @Mock private ISoftAssert softAssert;
@@ -76,56 +69,6 @@ class SetContextStepsTests
         assertThat(logger.getLoggingEvents(), is(empty()));
     }
 
-    @SuppressWarnings("removal")
-    @Test
-    void shouldSwitchToWebViewByIndex()
-    {
-        when(webDriverProvider.getUnwrapped(ContextAware.class)).thenReturn(contextAware);
-        when(contextAware.getContextHandles())
-                .thenReturn(Set.of(IGenericWebDriverManager.NATIVE_APP_CONTEXT, WEB_VIEW_MAIN));
-
-        setContextSteps.switchToWebViewByIndex(1);
-
-        verify(contextAware).context(WEB_VIEW_MAIN);
-        verifyNoMoreInteractions(softAssert, contextAware, webDriverProvider);
-        assertThat(logger.getLoggingEvents(), is(List.of(
-                DEPRECATION_NOTICE,
-                info(WEB_VIEWS_FOUND, Set.of(WEB_VIEW_MAIN).toString()),
-                info("Switching to '{}' web view found by the index {}", WEB_VIEW_MAIN, 1))));
-    }
-
-    @SuppressWarnings("removal")
-    @Test
-    void shouldNotSwitchToWebViewByIndexIfWebViewCollectionIsEmpty()
-    {
-        when(webDriverProvider.getUnwrapped(ContextAware.class)).thenReturn(contextAware);
-        when(contextAware.getContextHandles()).thenReturn(Set.of());
-
-        setContextSteps.switchToWebViewByIndex(1);
-
-        verify(softAssert).recordFailedAssertion("No web views found");
-        verifyNoMoreInteractions(softAssert, contextAware, webDriverProvider);
-        assertThat(logger.getLoggingEvents(), is(List.of(DEPRECATION_NOTICE)));
-    }
-
-    @SuppressWarnings("removal")
-    @ValueSource(ints = { 2, -2 })
-    @ParameterizedTest
-    void shouldNotSwitchToWebViewByIndexIfWebViewWithIndexDoesNotExist(int webViewIndex)
-    {
-        when(webDriverProvider.getUnwrapped(ContextAware.class)).thenReturn(contextAware);
-        when(contextAware.getContextHandles())
-                .thenReturn(Set.of(WEB_VIEW_MAIN, IGenericWebDriverManager.NATIVE_APP_CONTEXT));
-
-        setContextSteps.switchToWebViewByIndex(webViewIndex);
-
-        verify(softAssert).recordFailedAssertion(String.format("Web view with index %s does not exist", webViewIndex));
-        verifyNoMoreInteractions(softAssert, contextAware, webDriverProvider);
-        assertThat(logger.getLoggingEvents(), is(List.of(
-                DEPRECATION_NOTICE,
-                info(WEB_VIEWS_FOUND, Set.of(WEB_VIEW_MAIN).toString()))));
-    }
-
     @Test
     void shouldSwitchToWebViewByName()
     {
@@ -140,6 +83,19 @@ class SetContextStepsTests
         assertThat(logger.getLoggingEvents(), is(List.of(
                 info(WEB_VIEWS_FOUND, Set.of(WEB_VIEW_MAIN).toString()),
                 info("Switching to web view with the name '{}'", WEB_VIEW_MAIN))));
+    }
+
+    @Test
+    void shouldFailToSwitchToWebViewWhenNoWebViewIsAvailable()
+    {
+        when(webDriverProvider.getUnwrapped(ContextAware.class)).thenReturn(contextAware);
+        when(contextAware.getContextHandles()).thenReturn(Set.of(IGenericWebDriverManager.NATIVE_APP_CONTEXT));
+
+        setContextSteps.switchToWebViewByName(StringComparisonRule.IS_EQUAL_TO, WEB_VIEW_MAIN);
+
+        verify(softAssert).recordFailedAssertion("No web views found");
+        verifyNoMoreInteractions(softAssert, contextAware, webDriverProvider);
+        assertThat(logger.getLoggingEvents(), is(empty()));
     }
 
     @Test
