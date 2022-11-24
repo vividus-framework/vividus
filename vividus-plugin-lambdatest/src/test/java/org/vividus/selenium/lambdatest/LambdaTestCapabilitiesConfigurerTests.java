310d3116878ccd937d388667d25972f5921104cf
Valery Yatsynovich <valery_yatsynovich@epam.com>
2022-11-24 12:55:04 +0300

    [plugin-lambdatest] Switch to use W3C-protocol capabilities (#3386)

--- vividus-plugin-lambdatest/src/test/java/org/vividus/selenium/lambdatest/LambdaTestCapabilitiesConfigurerTests.java
+++ vividus-plugin-lambdatest/src/test/java/org/vividus/selenium/lambdatest/LambdaTestCapabilitiesConfigurerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 the original author or authors.
+ * Copyright 2019-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,6 +48,6 @@ class LambdaTestCapabilitiesConfigurerTests
         DesiredCapabilities capabilities = new DesiredCapabilities();
         configurer.configure(capabilities);
 
-        assertEquals(Map.of("name", storyName), capabilities.asMap());
+        assertEquals(Map.of("LT:Options", Map.of("name", storyName)), capabilities.asMap());
     }
 }
