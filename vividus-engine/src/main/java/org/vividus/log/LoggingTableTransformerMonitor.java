ee84c80e18cac20aa079707d6d63f2ebf258da5a
draker94 <58571912+draker94@users.noreply.github.com>
2022-11-30 16:44:23 +0300

    [engine] Add logging table transformer monitor (#3388)
    Co-authored-by: draker94 <noreply@github.com>

---
+++ vividus-engine/src/main/java/org/vividus/log/LoggingTableTransformerMonitor.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.log;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jbehave.core.model.ExamplesTable.TableProperties;
+import org.jbehave.core.model.TableParsers;
+import org.jbehave.core.model.TableTransformerMonitor;
+import org.jbehave.core.model.TableTransformers.Formatting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggingTableTransformerMonitor implements TableTransformerMonitor
+{
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingTableTransformerMonitor.class);
+
+    private static final Formatting FORMATTING_TRANSFORMER = new Formatting();
+    private static final int MAX_ROWS_LOGGING = 50;
+
+    private final TableParsers tableParsers;
+
+    public LoggingTableTransformerMonitor(TableParsers tableParsers)
+    {
+        this.tableParsers = tableParsers;
+    }
+
+    @Override
+    public void beforeTransformerApplying(String transformerName, TableProperties properties, String inputTable)
+    {
+        logTransformerInfo("Input table body", transformerName, properties, inputTable);
+    }
+
+    @Override
+    public void afterTransformerApplying(String transformerName, TableProperties properties, String outputTable)
+    {
+        logTransformerInfo("Output (transformed) table", transformerName, properties, outputTable);
+    }
+
+    private void logTransformerInfo(String tableTitle, String transformerName, TableProperties tableProperties,
+            String table)
+    {
+        LOGGER.atInfo()
+                .addArgument(transformerName)
+                .addArgument(() -> tableProperties.getPropertiesAsString().trim())
+                .addArgument(() -> prepareExampleTableForLogging(table, tableProperties))
+                .log(String.format("Applying table transformer: {}%nProperties: {{}}%n%s:%n{}", tableTitle));
+    }
+
+    private String prepareExampleTableForLogging(String originExampleTable, TableProperties tableProperties)
+    {
+        if (StringUtils.isBlank(originExampleTable))
+        {
+            return ">>>  EMPTY  <<<";
+        }
+
+        AtomicInteger totalNumberOrRows = new AtomicInteger();
+        String rowSeparator = tableProperties.getRowSeparator();
+        String resultingTable = originExampleTable.lines()
+                .filter(ignored -> totalNumberOrRows.incrementAndGet() <= MAX_ROWS_LOGGING)
+                .collect(Collectors.collectingAndThen(
+                        Collectors.joining(rowSeparator),
+                        table -> FORMATTING_TRANSFORMER.transform(table, tableParsers, tableProperties)
+                ))
+                .trim();
+        long numberOfTruncatedRows = totalNumberOrRows.get() - MAX_ROWS_LOGGING;
+        if (numberOfTruncatedRows > 0)
+        {
+            return String.format("%s%s...%d row(s) truncated", resultingTable, rowSeparator, numberOfTruncatedRows);
+        }
+        return resultingTable;
+    }
+}
---
+++ vividus-engine/src/test/java/org/vividus/log/LoggingTableTransformerMonitorTests.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2019-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.log;
+
+import static com.github.valfirst.slf4jtest.LoggingEvent.info;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyNoInteractions;
+
+import java.util.List;
+
+import com.github.valfirst.slf4jtest.TestLogger;
+import com.github.valfirst.slf4jtest.TestLoggerFactory;
+import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
+
+import org.jbehave.core.configuration.Keywords;
+import org.jbehave.core.model.ExamplesTable.TableProperties;
+import org.jbehave.core.model.TableParsers;
+import org.jbehave.core.steps.ParameterConverters;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import uk.org.lidalia.slf4jext.Level;
+
+@SuppressWarnings({"MultipleStringLiterals", "MultipleStringLiteralsExtended"})
+@ExtendWith(TestLoggerFactoryExtension.class)
+class LoggingTableTransformerMonitorTests
+{
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+    private static final String TRANSFORMER_NAME = "TEST_TRANSFORMER";
+    private static final String BEFORE_APPLYING_FORMAT = "Applying table transformer: {}" + LINE_SEPARATOR
+                                                       + "Properties: {{}}" + LINE_SEPARATOR
+                                                       + "Input table body:" + LINE_SEPARATOR
+                                                       + "{}";
+    private static final String AFTER_APPLYING_FORMAT = "Applying table transformer: {}" + LINE_SEPARATOR
+                                                      + "Properties: {{}}" + LINE_SEPARATOR
+                                                      + "Output (transformed) table:" + LINE_SEPARATOR
+                                                      + "{}";
+    private static final String EXAMPLE_TABLE = "|Earth|Moon  |\n"
+                                              + "!Mars !Deimos!\n";
+    private static final String LOGGED_EXAMPLE_TABLE = "|Earth|Moon  |\n"
+                                                     + "!Mars !Deimos!";
+    private static final String LOGGED_PROPERTIES = "transformer=TEST_TRANSFORMER, valueSeparator=!";
+    private static final int MAX_ROWS_LOGGING = 50;
+
+    private final TestLogger logger = TestLoggerFactory.getTestLogger(LoggingTableTransformerMonitor.class);
+    private final Keywords keywords = new Keywords();
+    private final ParameterConverters parameterConverters = new ParameterConverters();
+    private final TableParsers tableParsers = new TableParsers(parameterConverters);
+    private final LoggingTableTransformerMonitor monitor = new LoggingTableTransformerMonitor(tableParsers);
+    private final TableProperties tableProperties = new TableProperties(
+            "transformer=TEST_TRANSFORMER, valueSeparator=!", keywords, parameterConverters);
+
+    @Test
+    void shouldLogTableTransformerBeforeAndAfter()
+    {
+        monitor.beforeTransformerApplying(TRANSFORMER_NAME, tableProperties, EXAMPLE_TABLE);
+        monitor.afterTransformerApplying(TRANSFORMER_NAME, tableProperties, EXAMPLE_TABLE);
+
+        assertThat(logger.getLoggingEvents(), is(List.of(
+                info(BEFORE_APPLYING_FORMAT, TRANSFORMER_NAME, LOGGED_PROPERTIES, LOGGED_EXAMPLE_TABLE),
+                info(AFTER_APPLYING_FORMAT, TRANSFORMER_NAME, LOGGED_PROPERTIES, LOGGED_EXAMPLE_TABLE)
+        )));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'',       |header|, >>>  EMPTY  <<<,    |header|",
+            "|header|, '',       |header|,           >>>  EMPTY  <<<"
+    })
+    void shouldLogTableTransformerBeforeAndAfterIfTablesEmpty(String inputTable, String outputTable,
+            String loggedInputTable, String loggedOutputTable)
+    {
+        monitor.beforeTransformerApplying(TRANSFORMER_NAME, tableProperties, inputTable);
+        monitor.afterTransformerApplying(TRANSFORMER_NAME, tableProperties, outputTable);
+
+        assertThat(logger.getLoggingEvents(), is(List.of(
+                info(BEFORE_APPLYING_FORMAT, TRANSFORMER_NAME, LOGGED_PROPERTIES, loggedInputTable),
+                info(AFTER_APPLYING_FORMAT, TRANSFORMER_NAME, LOGGED_PROPERTIES, loggedOutputTable)
+        )));
+    }
+
+    @Test
+    void shouldLogTableTransformerWithTruncatedTable()
+    {
+        var rowsCountWithoutHeader = 51;
+        var rowHeader = "|header|\n";
+        var rowLine = "!row   !\n";
+        var transformerTable = rowHeader + rowLine.repeat(rowsCountWithoutHeader);
+        var loggedTable = rowHeader + rowLine.repeat(MAX_ROWS_LOGGING - 1).trim() + "\n...2 row(s) truncated";
+
+        monitor.afterTransformerApplying(TRANSFORMER_NAME, tableProperties, transformerTable);
+
+        assertThat(logger.getLoggingEvents(), is(List.of(
+                info(AFTER_APPLYING_FORMAT, TRANSFORMER_NAME, LOGGED_PROPERTIES, loggedTable)
+        )));
+    }
+
+    @Test
+    void shouldNotProcessTransformerIfLoggerLevelInfoNotUsing()
+    {
+        var tablePropertiesMocked = mock(TableProperties.class);
+
+        logger.setEnabledLevels(Level.DEBUG);
+        monitor.beforeTransformerApplying(TRANSFORMER_NAME, tablePropertiesMocked, EXAMPLE_TABLE);
+        monitor.afterTransformerApplying(TRANSFORMER_NAME, tablePropertiesMocked, EXAMPLE_TABLE);
+
+        verifyNoInteractions(tablePropertiesMocked);
+    }
+}
