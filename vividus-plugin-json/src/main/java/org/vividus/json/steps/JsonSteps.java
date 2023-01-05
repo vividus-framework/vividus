fd3ac0463646235d032b9de9c58d8ad4c50cdd42
draker94 <58571912+draker94@users.noreply.github.com>
2023-01-05 13:32:24 +0300

    [plugin-json] Fix broken JSON assert with `IGNORING_EXTRA_ARRAY_ITEMS` option (#3522)
    Co-authored-by: draker94 <noreply@github.com>

--- vividus-plugin-json/src/main/java/org/vividus/json/steps/JsonSteps.java
+++ vividus-plugin-json/src/main/java/org/vividus/json/steps/JsonSteps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,7 +56,7 @@ public class JsonSteps
 {
     private static final String ASSERTION_BOUNDS = String.join("|", Set.of(
             "Different (?:value|keys) found",
-            "Array \"[^\"]*\" has different"
+            "Array \"[^\"]*\" has (different|invalid)"
     ));
     private static final char LF = '\n';
     private static final Pattern DIFFERENCES_PATTERN = Pattern.compile(
