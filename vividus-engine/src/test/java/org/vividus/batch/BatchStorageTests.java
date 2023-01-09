7f0734e7c8f527a6affa1c44137a587b0771fdfe
Ivan Kalinin <ivan_kalinin1@epam.com>
2023-01-09 13:59:13 +0300

    [engine] Align story execution timeout properties naming

--- vividus-engine/src/test/java/org/vividus/batch/BatchStorageTests.java
+++ vividus-engine/src/test/java/org/vividus/batch/BatchStorageTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.vividus.batch;
 
+import static com.github.valfirst.slf4jtest.LoggingEvent.warn;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -35,13 +36,17 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.github.valfirst.slf4jtest.TestLogger;
+import com.github.valfirst.slf4jtest.TestLoggerFactory;
+import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
 
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.vividus.util.property.PropertyMapper;
 import org.vividus.util.property.PropertyParser;
 
+@ExtendWith(TestLoggerFactoryExtension.class)
 class BatchStorageTests
 {
     private static final List<String> BATCH_NUMBERS = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
@@ -49,10 +54,11 @@ class BatchStorageTests
     private static final List<String> BATCH_KEYS = BATCH_NUMBERS.stream().map(n -> BATCH + n).collect(
             Collectors.toList());
 
+    private static final Duration EXPECTED_DURATION = Duration.ofSeconds(300);
     private static final String DEFAULT_RESOURCE_LOCATION = "";
     private static final String TRUE = "true";
+    private static final String DOT = ".";
 
-    private static final long DEFAULT_TIMEOUT = 300;
     private static final List<String> DEFAULT_META_FILTERS = List.of("groovy: !skip");
 
     private static final String BATCH_2_NAME = "second batch name";
@@ -62,10 +68,11 @@ class BatchStorageTests
     private static final String VALUE_1 = "value1";
     private static final String VALUE_2 = "value2";
 
+    private static final TestLogger LOGGER = TestLoggerFactory.getTestLogger(BatchStorage.class);
+
     private BatchStorage batchStorage;
 
-    @BeforeEach
-    void beforeEach() throws IOException
+    void initializeBatchStorage() throws IOException
     {
         var batchConfigurations = new HashMap<String, String>();
         addBatchConfiguration(batchConfigurations, 1);
@@ -95,7 +102,7 @@ class BatchStorageTests
         batchConfigurations.put("batch-2.story.fail-fast", TRUE);
         batchConfigurations.put("batch-2.name", BATCH_2_NAME);
         batchConfigurations.put("batch-2.threads", Integer.toString(BATCH_2_THREADS));
-        batchConfigurations.put("batch-2.story-execution-timeout", BATCH_2_TIMEOUT.toString());
+        batchConfigurations.put("batch-2.story.execution-timeout", BATCH_2_TIMEOUT.toString());
         batchConfigurations.put("batch-2.meta-filters", BATCH_2_META_FILTERS);
         batchConfigurations.put("batch-2.fail-fast", TRUE);
         batchConfigurations.put("batch-2.variables.key1", VALUE_1);
@@ -103,13 +110,14 @@ class BatchStorageTests
         var propertyParser = mock(PropertyParser.class);
         when(propertyParser.getPropertiesByPrefix(BATCH)).thenReturn(batchConfigurations);
 
-        var propertyMapper = new PropertyMapper(".", PropertyNamingStrategies.KEBAB_CASE, propertyParser, Set.of());
-        batchStorage = new BatchStorage(propertyMapper, Long.toString(DEFAULT_TIMEOUT), DEFAULT_META_FILTERS, false);
+        var propertyMapper = new PropertyMapper(DOT, PropertyNamingStrategies.KEBAB_CASE, propertyParser, Set.of());
+        batchStorage = new BatchStorage(propertyMapper, EXPECTED_DURATION, null, DEFAULT_META_FILTERS, false);
     }
 
     @Test
-    void shouldGetBatchConfigurationByKey()
+    void shouldGetBatchConfigurationByKey() throws IOException
     {
+        initializeBatchStorage();
         var batchConfiguration = batchStorage.getBatchConfiguration(BATCH_KEYS.get(0));
         assertAll(
             () -> assertEquals(DEFAULT_RESOURCE_LOCATION, batchConfiguration.getResourceLocation()),
@@ -119,21 +127,24 @@ class BatchStorageTests
     }
 
     @Test
-    void shouldGetAllBatchConfigurations()
+    void shouldGetAllBatchConfigurations() throws IOException
     {
+        initializeBatchStorage();
         Map<String, BatchConfiguration> allBatches = batchStorage.getBatchConfigurations();
         assertThat(allBatches.keySet(), Matchers.contains(BATCH_KEYS.toArray()));
     }
 
     @Test
-    void shouldGetBatchConfigurationInitializedWithDefaultValues()
+    void shouldGetBatchConfigurationInitializedWithDefaultValues() throws IOException
     {
+        initializeBatchStorage();
         assertDefaultBatchConfiguration(BATCH_KEYS.get(0));
     }
 
     @Test
-    void shouldGetBatchConfigurationInitializedWithNonDefaultValues()
+    void shouldGetBatchConfigurationInitializedWithNonDefaultValues() throws IOException
     {
+        initializeBatchStorage();
         var config = batchStorage.getBatchConfiguration(BATCH_KEYS.get(1));
         assertAll(
             () -> assertEquals(BATCH_2_NAME, config.getName()),
@@ -148,8 +159,9 @@ class BatchStorageTests
     }
 
     @Test
-    void shouldCreateNonExistentBatchConfiguration()
+    void shouldCreateNonExistentBatchConfiguration() throws IOException
     {
+        initializeBatchStorage();
         assertDefaultBatchConfiguration("batch-100");
     }
 
@@ -159,7 +171,7 @@ class BatchStorageTests
         assertAll(
             () -> assertEquals(batchKey,  config.getName()),
             () -> assertNull(config.getThreads()),
-            () -> assertEquals(Duration.ofSeconds(DEFAULT_TIMEOUT), config.getStoryExecutionTimeout()),
+            () -> assertEquals(EXPECTED_DURATION, config.getStoryExecutionTimeout()),
             () -> assertEquals(DEFAULT_META_FILTERS, config.getMetaFilters()),
             () -> assertFalse(config.isFailFast()),
             () -> assertNull(config.isFailStoryFast()),
@@ -169,12 +181,56 @@ class BatchStorageTests
     }
 
     @Test
-    void shouldThrowErrorIfBatchResourceConfigurationDoesNotContainResourceLocation()
+    void shouldThrowErrorIfBatchResourceConfigurationDoesNotContainResourceLocation() throws IOException
     {
+        initializeBatchStorage();
         var batchConfigurations = new HashMap<String, String>();
         batchConfigurations.put("batch-1.resource-include-patterns", "*.story");
         var exception = assertThrows(IllegalArgumentException.class,
                 () -> createBatchStorage(batchConfigurations));
         assertEquals("'resource-location' is missing for batch-1", exception.getMessage());
+    }
+
+    @Test
+    void shouldWarnAboutPlainSecondsUsage() throws IOException
+    {
+        var propertyParser = mock(PropertyParser.class);
+        when(propertyParser.getPropertiesByPrefix(BATCH)).thenReturn(Map.of("batch-999.resource-location",
+            DEFAULT_RESOURCE_LOCATION));
+
+        var propertyMapper = new PropertyMapper(DOT, PropertyNamingStrategies.KEBAB_CASE, propertyParser, Set.of());
+        batchStorage = new BatchStorage(propertyMapper, null, "300", DEFAULT_META_FILTERS, false);
+        assertEquals(List.of(warn("Property `bdd.story-execution-timeout` is deprecated and will be removed"
+            + " in VIVIDUS 0.7.0. Please use `story.execution-timeout` instead.")), LOGGER.getLoggingEvents());
+        var config = batchStorage.getBatchConfiguration("batch-999");
+        assertEquals(EXPECTED_DURATION, config.getStoryExecutionTimeout());
+    }
+
+    @Test
+    void shouldThrowExceptionWhenBothPropertiesSpecified()
+    {
+        var propertyParser = mock(PropertyParser.class);
+        when(propertyParser.getPropertiesByPrefix(BATCH)).thenReturn(Map.of("batch-998.resource-location",
+                DEFAULT_RESOURCE_LOCATION));
+        var duration = Duration.ofHours(111);
+        var propertyMapper = new PropertyMapper(DOT, PropertyNamingStrategies.KEBAB_CASE, propertyParser, Set.of());
+        var iae = assertThrows(IllegalArgumentException.class,
+            () -> new BatchStorage(propertyMapper, duration, "202", DEFAULT_META_FILTERS, false));
+        assertEquals(iae.getMessage(), "Conflicting properties are found: `bdd.story-execution-timeout`"
+            + " and `story.execution-timeout`. Property `bdd.story-execution-timeout` is deprecated and will be"
+            + " removed in VIVIDUS 0.7.0. Please use `story.execution-timeout` instead.");
+    }
+
+    @Test
+    void shouldUseDefaultTimeout() throws IOException
+    {
+        var propertyParser = mock(PropertyParser.class);
+        when(propertyParser.getPropertiesByPrefix(BATCH)).thenReturn(Map.of("batch-997.resource-location",
+                DEFAULT_RESOURCE_LOCATION));
+
+        var propertyMapper = new PropertyMapper(DOT, PropertyNamingStrategies.KEBAB_CASE, propertyParser, Set.of());
+        batchStorage = new BatchStorage(propertyMapper, null, null, DEFAULT_META_FILTERS, false);
+        var config = batchStorage.getBatchConfiguration("batch-997");
+        assertEquals(Duration.ofHours(3), config.getStoryExecutionTimeout());
     }
 }
