dc36289bb7fb0f9838ab1a94d0945f6562fef43e
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-01-17 20:57:31 +0300

    [plugin-web-app] Update tests for varargs method to address Mockito changes

--- vividus-plugin-web-app/src/test/java/org/vividus/ui/web/listener/WebSourceCodePublishingOnFailureListenerTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/ui/web/listener/WebSourceCodePublishingOnFailureListenerTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -111,7 +111,7 @@ class WebSourceCodePublishingOnFailureListenerTests
     void shouldReturnEmptyValueForNullSearchContext()
     {
         when(uiContext.getSearchContext()).thenReturn(null);
-        when(webJavascriptActions.executeScript(any(String.class), any())).thenReturn(Map.of());
+        when(webJavascriptActions.executeScript(any(String.class))).thenReturn(Map.of());
         assertEquals(Map.of(), listener.getSourceCode());
     }
 
