cb287b1811c1859368c7f11a6dcad3791fedb530
Ivan Baranau <ivan_baranau@epam.com>
2023-01-27 11:59:29 +0300

    [plugin-mobile-app] Add shooting strategy to take fullscreen screenshot

--- vividus-plugin-mobile-app/src/test/java/org/vividus/selenium/mobileapp/screenshot/MobileAppAshotFactoryTests.java
+++ vividus-plugin-mobile-app/src/test/java/org/vividus/selenium/mobileapp/screenshot/MobileAppAshotFactoryTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,33 @@
 
 package org.vividus.selenium.mobileapp.screenshot;
 
+import static com.github.valfirst.slf4jtest.LoggingEvent.warn;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertInstanceOf;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
+
+import com.github.valfirst.slf4jtest.TestLogger;
+import com.github.valfirst.slf4jtest.TestLoggerFactory;
+import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
 
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.vividus.selenium.mobileapp.MobileAppWebDriverManager;
 import org.vividus.selenium.mobileapp.screenshot.strategies.MobileViewportShootingStrategy;
-import org.vividus.selenium.mobileapp.screenshot.strategies.SimpleScreenshotShootingStrategy;
 import org.vividus.ui.screenshot.ScreenshotParameters;
 
 import pazone.ashot.AShot;
@@ -44,27 +54,31 @@ import pazone.ashot.SimpleShootingStrategy;
 import pazone.ashot.coordinates.CoordsProvider;
 import pazone.ashot.cutter.CutStrategy;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, TestLoggerFactoryExtension.class})
 class MobileAppAshotFactoryTests
 {
     private static final String CUT_STRATEGY = "cutStrategy";
     private static final String SHOOTING_STRATEGY = "shootingStrategy";
-    private static final String DIMPLE = "dimple";
     private static final String SIMPLE = "SIMPLE";
+    private static final String VIEWPORT = "VIEWPORT";
+    private static final String FULL_SCREEN = "FULL_SCREEN";
+
+    private final TestLogger logger = TestLoggerFactory.getTestLogger(MobileAppAshotFactory.class);
 
     @Mock private MobileAppWebDriverManager mobileAppWebDriverManager;
     @Mock private CoordsProvider coordsProvider;
     @InjectMocks private MobileAppAshotFactory ashotFactory;
 
-    @Test
-    void shouldCreateAshotWithMergedConfiguration() throws IllegalAccessException
+    @ParameterizedTest
+    @ValueSource(strings = { SIMPLE, VIEWPORT })
+    void shouldCreateAshotWithMergedConfiguration(String strategyName) throws IllegalAccessException
     {
-        mockAshotConfiguration(true);
+        ashotFactory.setDownscale(true);
         when(mobileAppWebDriverManager.getDpr()).thenReturn(2d);
         ashotFactory.setAppendBottomNavigationBarOnAndroid(true);
         when(mobileAppWebDriverManager.isAndroid()).thenReturn(false);
         var parameters = new ScreenshotParameters();
-        parameters.setShootingStrategy(Optional.of(SIMPLE));
+        parameters.setShootingStrategy(Optional.of(strategyName));
         var aShot = ashotFactory.create(Optional.of(parameters));
         var croppingDecorator = (ElementCroppingDecorator) FieldUtils.readField(aShot, SHOOTING_STRATEGY, true);
         var strategy = (ScalingDecorator) FieldUtils.readField(croppingDecorator, SHOOTING_STRATEGY, true);
@@ -75,7 +89,7 @@ class MobileAppAshotFactoryTests
     @Test
     void shouldCreateAshotWithDefaultConfigurationWithoutBottomNavigationBar() throws IllegalAccessException
     {
-        mockAshotConfiguration(false);
+        ashotFactory.setDownscale(false);
         ashotFactory.setAppendBottomNavigationBarOnAndroid(false);
         ashotFactory.setScreenshotShootingStrategy(SIMPLE);
         var aShot = ashotFactory.create(Optional.empty());
@@ -86,7 +100,7 @@ class MobileAppAshotFactoryTests
     @Test
     void shouldCreateAshotWithDefaultConfigurationWithBottomNavigationBarOnAndroid() throws IllegalAccessException
     {
-        mockAshotConfiguration(false);
+        ashotFactory.setDownscale(false);
         when(mobileAppWebDriverManager.getStatusBarSize()).thenReturn(2);
         ashotFactory.setAppendBottomNavigationBarOnAndroid(true);
         when(mobileAppWebDriverManager.isAndroid()).thenReturn(true);
@@ -101,12 +115,47 @@ class MobileAppAshotFactoryTests
         assertCoordsProvider(aShot);
     }
 
-    private void mockAshotConfiguration(boolean downscale)
+    @Test
+    void shouldCreateAshotFullScreenStrategyWithMergedConfiguration() throws IllegalAccessException
     {
-        ashotFactory.setDownscale(downscale);
-        ashotFactory.setStrategies(Map.of(SIMPLE, new SimpleScreenshotShootingStrategy(), DIMPLE, s -> {
-            throw new IllegalStateException();
-        }));
+        var parameters = new ScreenshotParameters();
+        parameters.setShootingStrategy(Optional.of(FULL_SCREEN));
+        var aShot = ashotFactory.create(Optional.of(parameters));
+        var croppingDecorator = (ElementCroppingDecorator) FieldUtils.readField(aShot, SHOOTING_STRATEGY, true);
+        assertInstanceOf(SimpleShootingStrategy.class,
+                FieldUtils.readField(croppingDecorator, SHOOTING_STRATEGY, true));
+        assertCoordsProvider(aShot);
+    }
+
+    @Test
+    void shouldWarnAboutDeprecatedStrategy()
+    {
+        var parameters = new ScreenshotParameters();
+        parameters.setShootingStrategy(Optional.of(SIMPLE));
+        ashotFactory.create(Optional.of(parameters));
+        assertThat(logger.getLoggingEvents(), is(List.of(
+                warn("Shooting strategy '{}' is deprecated and will be removed in VIVIDUS 0.6.0. Use '{}' instead",
+                        SIMPLE, VIEWPORT))));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { FULL_SCREEN, VIEWPORT })
+    void shouldNotWarnAboutDeprecatedStrategy(String strategyName)
+    {
+        var parameters = new ScreenshotParameters();
+        parameters.setShootingStrategy(Optional.of(strategyName));
+        ashotFactory.create(Optional.of(parameters));
+        assertTrue(logger.getLoggingEvents().isEmpty());
+    }
+
+    @Test
+    void shouldThrowAnExceptionIfThereIsNoStrategyByTheName()
+    {
+        var parameters = new ScreenshotParameters();
+        parameters.setShootingStrategy(Optional.of("dimple"));
+        var exception = assertThrows(IllegalArgumentException.class,
+                () -> ashotFactory.create(Optional.of(parameters)));
+        assertEquals("Unknown shooting strategy with the name: dimple", exception.getMessage());
     }
 
     private void assertCoordsProvider(AShot aShot) throws IllegalAccessException
