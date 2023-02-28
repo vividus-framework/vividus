6dca766ac6847a95c708d9b72d3ad372a50aacec
Uladzislau Arlouski <uarlouski@gmail.com>
2023-02-28 18:37:34 +0200

    [plugin-kafka] Aligh syntax with the official documentation

--- vividus-plugin-kafka/src/test/java/org/vividus/steps/kafka/KafkaStepsIntegrationTests.java
+++ vividus-plugin-kafka/src/test/java/org/vividus/steps/kafka/KafkaStepsIntegrationTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 package org.vividus.steps.kafka;
 
 import static com.github.valfirst.slf4jtest.LoggingEvent.info;
+import static com.github.valfirst.slf4jtest.LoggingEvent.warn;
 import static java.util.stream.Collectors.toMap;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -35,6 +36,7 @@ import java.util.concurrent.TimeoutException;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
+import com.github.valfirst.slf4jtest.LoggingEvent;
 import com.github.valfirst.slf4jtest.TestLogger;
 import com.github.valfirst.slf4jtest.TestLoggerFactory;
 import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
@@ -78,6 +80,12 @@ class KafkaStepsIntegrationTests
 
     private static final String VARIABLE_NAME = "var";
 
+    private static final String EQUAL_TO_ONE_MATCHER = "a value equal to <1>";
+
+    private static final String LISTENER_STARTED = "Kafka event listener is started";
+
+    private static final String LISTENER_STOPPED = "Kafka event listener is stopped";
+
     private static final Set<VariableScope> SCOPES = Set.of(VariableScope.SCENARIO);
 
     private final TestLogger logger = TestLoggerFactory.getTestLogger(KafkaSteps.class);
@@ -113,36 +121,60 @@ class KafkaStepsIntegrationTests
     {
         return Stream.of(
                 Arguments.of((BiConsumer<KafkaSteps, VariableContext>) (steps, context) -> {
-                    steps.processKafkaMessages(QueueOperation.DRAIN, CONSUMER, SCOPES, VARIABLE_NAME);
-                    steps.processKafkaMessages(QueueOperation.DRAIN, CONSUMER, SCOPES, VARIABLE_NAME);
+                    steps.processKafkaEvents(QueueOperation.DRAIN, CONSUMER, SCOPES, VARIABLE_NAME);
+                    steps.processKafkaEvents(QueueOperation.DRAIN, CONSUMER, SCOPES, VARIABLE_NAME);
                     InOrder ordered = Mockito.inOrder(context);
                     ordered.verify(context).putVariable(SCOPES, VARIABLE_NAME, List.of(ANY_DATA));
                     ordered.verify(context).putVariable(SCOPES, VARIABLE_NAME, List.of());
                 }),
                 Arguments.of((BiConsumer<KafkaSteps, VariableContext>) (steps, context) -> {
-                    steps.processKafkaMessages(QueueOperation.PEEK, CONSUMER, SCOPES, VARIABLE_NAME);
-                    steps.processKafkaMessages(QueueOperation.PEEK, CONSUMER, SCOPES, VARIABLE_NAME);
+                    steps.processKafkaEvents(QueueOperation.PEEK, CONSUMER, SCOPES, VARIABLE_NAME);
+                    steps.processKafkaEvents(QueueOperation.PEEK, CONSUMER, SCOPES, VARIABLE_NAME);
                     verify(context, times(2)).putVariable(SCOPES, VARIABLE_NAME, List.of(ANY_DATA));
                 }));
     }
 
     @ParameterizedTest
     @MethodSource("kafkaOperations")
-    void shouldProduceToAndConsumerFromKafka(BiConsumer<KafkaSteps, VariableContext> test)
+    void shouldProduceMessagesToAndConsumeMessagesFromKafka(BiConsumer<KafkaSteps, VariableContext> test)
             throws InterruptedException, ExecutionException, TimeoutException
     {
         kafkaSteps.startKafkaListener(CONSUMER, Set.of(TOPIC));
 
-        kafkaSteps.sendData(ANY_DATA, PRODUCER, TOPIC);
+        kafkaSteps.sendEvent(ANY_DATA, PRODUCER, TOPIC);
 
         ComparisonRule comparisonRule = ComparisonRule.EQUAL_TO;
         kafkaSteps.waitForKafkaMessages(Duration.ofSeconds(10), CONSUMER, comparisonRule, 1);
         verify(softAssert).assertThat(eq("Total count of consumed Kafka messages"), eq(1),
-                argThat(matcher -> "a value equal to <1>".equals(matcher.toString())));
+                argThat(matcher -> EQUAL_TO_ONE_MATCHER.equals(matcher.toString())));
 
         kafkaSteps.stopKafkaListener(CONSUMER);
-        assertThat(logger.getLoggingEvents(),
-                is(List.of(info("Kafka message listener is started"), info("Kafka message listener is stopped"))));
+
+        LoggingEvent deprecatedStep = warn("The step: \"When I wait with `$timeout` timeout until count of consumed "
+                + "`$consumerKey` Kafka events is $comparisonRule `$expectedCount`\" is deprecated and will be removed"
+                + " in VIVIDUS 0.6.0. Use step: \"`$expectedCount`\"");
+        assertThat(logger.getLoggingEvents(), is(List.of(info(LISTENER_STARTED), deprecatedStep,
+                info(LISTENER_STOPPED))));
+
+        test.accept(kafkaSteps, variableContext);
+    }
+
+    @ParameterizedTest
+    @MethodSource("kafkaOperations")
+    void shouldProduceEventsToAndConsumeEventsFromKafka(BiConsumer<KafkaSteps, VariableContext> test)
+            throws InterruptedException, ExecutionException, TimeoutException
+    {
+        kafkaSteps.startKafkaListener(CONSUMER, Set.of(TOPIC));
+
+        kafkaSteps.sendEvent(ANY_DATA, PRODUCER, TOPIC);
+
+        ComparisonRule comparisonRule = ComparisonRule.EQUAL_TO;
+        kafkaSteps.waitForKafkaEvents(Duration.ofSeconds(10), CONSUMER, comparisonRule, 1);
+        verify(softAssert).assertThat(eq("Total count of consumed Kafka events"), eq(1),
+                argThat(matcher -> EQUAL_TO_ONE_MATCHER.equals(matcher.toString())));
+
+        kafkaSteps.stopKafkaListener(CONSUMER);
+        assertThat(logger.getLoggingEvents(), is(List.of(info(LISTENER_STARTED), info(LISTENER_STOPPED))));
 
         test.accept(kafkaSteps, variableContext);
     }
