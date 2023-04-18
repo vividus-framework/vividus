e2a52e9dac4c7df8ba18bc02ecb44eb4b4029a50
VolhaHurynovich <Volha_Hurynovich@epam.com>
2023-04-18 09:41:25 +0300

    [vividus] Add optional sortingTypes parameter to SORTING transformer

--- vividus/src/test/java/org/vividus/transformer/SortingTableTransformerTests.java
+++ vividus/src/test/java/org/vividus/transformer/SortingTableTransformerTests.java
@@ -35,8 +35,9 @@ import org.vividus.converter.FluentTrimmedEnumConverter;
 class SortingTableTransformerTests
 {
     private static final String TABLE_WITH_SAME_VALUES = "|key1|key2|\n|10|0|\n|1|0|";
-    private static final String TABLE = "|key1|key2|\n|4|3|\n|1|0|";
-    private static final String REVERSE_TABLE = "|key1|key2|\n|1|0|\n|4|3|";
+    private static final String TABLE = "|key1|key2|\n|4|3|\n|10|0|";
+    private static final String REVERSE_TABLE = "|key1|key2|\n|10|0|\n|4|3|";
+    private static final String TABLE_STRING = "|key1|key2|key3|\n|4a|3|4|\n|10|0|1|";
 
     private final Keywords keywords = new Keywords();
     private final ParameterConverters parameterConverters = new ParameterConverters();
@@ -47,11 +48,15 @@ class SortingTableTransformerTests
     // CHECKSTYLE:OFF
     static Stream<Arguments> tableSource() {
         return Stream.of(
-                arguments(TABLE_WITH_SAME_VALUES,              "byColumns=key2",                  TABLE_WITH_SAME_VALUES),
-                arguments("|key1|key2|key3|\n|0|2|1|\n|0|2|5|","byColumns=key1|key3",             "|key1|key2|key3|\n|0|2|5|\n|0|2|1|"),
-                arguments(TABLE,                               "byColumns=key5, order=ascending", TABLE),
-                arguments(REVERSE_TABLE,                       "byColumns=key1, order=ASCENDING", TABLE),
-                arguments(TABLE,                               "byColumns=key1, order=DESCENDING",REVERSE_TABLE)
+                arguments(TABLE_WITH_SAME_VALUES,              "byColumns=key2",                                         TABLE_WITH_SAME_VALUES),
+                arguments("|key1|key2|key3|\n|0|2|1|\n|0|2|5|","byColumns=key1|key3",                                    "|key1|key2|key3|\n|0|2|5|\n|0|2|1|"),
+                arguments(TABLE_STRING,                        "byColumns=key5, order=ascending",                        TABLE_STRING),
+                arguments(REVERSE_TABLE,                       "byColumns=key1, order=ASCENDING",                        TABLE),
+                arguments(TABLE,                               "byColumns=key1, order=DESCENDING",                       REVERSE_TABLE),
+                arguments(TABLE,                               "byColumns=key1, order=DESCENDING",                       REVERSE_TABLE),
+                arguments(TABLE,                               "byColumns=key1, order=ASCENDING, sortingTypes = number", TABLE),
+                arguments(REVERSE_TABLE,                "byColumns=key1, order=ASCENDING, sortingTypes = string",        TABLE),
+                arguments("|key1|key2|key3|\n|a|2|1|\n|a|2|5|","byColumns=key1|key3, sortingTypes = string|number",      "|key1|key2|key3|\n|a|2|5|\n|a|2|1|")
         );
     }
     // CHECKSTYLE:ON
@@ -71,5 +76,28 @@ class SortingTableTransformerTests
         var exception = assertThrows(IllegalArgumentException.class,
             () -> transformer.transform(TABLE, tableParsers, tableProperties));
         assertEquals("'byColumns' is not set in ExamplesTable properties", exception.getMessage());
+    }
+
+    @Test
+    void testFailOnUnsupportedValueForNumber()
+    {
+        var tableProperties = new TableProperties("byColumns=key1, sortingTypes = NUMBER", keywords,
+                parameterConverters);
+        var exception = assertThrows(IllegalArgumentException.class,
+            () -> transformer.transform(TABLE_STRING, tableParsers, tableProperties));
+        assertEquals("NUMBER sorting type supports only number values for comparison. Character a is neither a"
+                + " decimal digit number, decimal point, nor \"e\" notation exponential mark.", exception.getMessage());
+    }
+
+    @Test
+    void testFailOnDifferentCountForSortingTypesAndByColumns()
+    {
+        var tableProperties = new TableProperties("byColumns=key1|key2|key3, sortingTypes = String|number", keywords,
+                parameterConverters);
+        var exception = assertThrows(IllegalArgumentException.class,
+            () -> transformer.transform(TABLE_STRING, tableParsers, tableProperties));
+        assertEquals("Please, specify parameter 'sortingType' (String|number) with the same count of types as count"
+                + " of column names in parameter 'byColumns' (key1|key2|key3)",
+                exception.getMessage());
     }
 }
