2664a99ad81242505a9e3f9c3e18bfb8eae2ae7a
draker94 <noreply@github.com>
2023-06-26 18:23:40 +0300

    [extension-selenium] Move wait step with polling timeout to generic steps

--- vividus-extension-selenium/src/main/java/org/vividus/steps/ui/GenericWaitSteps.java
+++ vividus-extension-selenium/src/main/java/org/vividus/steps/ui/GenericWaitSteps.java
@@ -34,6 +34,7 @@ import org.slf4j.LoggerFactory;
 import org.vividus.softassert.ISoftAssert;
 import org.vividus.steps.ComparisonRule;
 import org.vividus.steps.ui.validation.IBaseValidations;
+import org.vividus.ui.State;
 import org.vividus.ui.action.IExpectedConditions;
 import org.vividus.ui.action.IExpectedSearchContextCondition;
 import org.vividus.ui.action.ISearchActions;
@@ -99,6 +100,37 @@ public class GenericWaitSteps
             "The step supports locators with VISIBLE visibility settings only, but the locator is `%s`",
                 locator.toHumanReadableString());
         waitForCondition(conditionFactory.apply(locator));
+    }
+
+    /**
+     * Waits <b>duration</b> with <b>pollingDuration</b> until <b>an element</b> by the specified <b>locator</b>
+     * becomes a <b>state</b> in the specified search context
+     * <p>
+     * Actions performed at this step:
+     * </p>
+     * <ul>
+     * <li><i>Finds</i> an element by the specified <b>locator</b> within search context
+     * <li><i>Waits</i> <b>duration</b> with <b>pollingDuration</b> until this element becomes <b>state</b>
+     * </ul>
+     * <br>Example:<br>
+     * <code>When I wait 'PT30S' with 'PT10S' polling until element located `By.id(text)` becomes NOT_VISIBLE</code>
+     * - wait until all elements with id=text becomes not visible for 30 seconds, polling every 10 seconds
+     *
+     * @param duration        Total waiting time according to
+     *                        <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a> standard
+     * @param pollingDuration Defines the timeout between attempts according to
+     *                        <a href="https://en.wikipedia.org/wiki/ISO_8601">ISO 8601</a> standard
+     * @param locator         Locator to search for elements
+     * @param state           State value of the element
+     * (<i>Possible values:</i> <b>ENABLED, DISABLED, SELECTED, NOT_SELECTED, VISIBLE, NOT_VISIBLE</b>)
+     * @return True if element becomes a <b>state</b>, otherwise false
+     */
+    @When("I wait `$duration` with `$pollingDuration` polling until element located by `$locator` becomes $state")
+    public boolean waitDurationWithPollingDurationTillElementState(Duration duration, Duration pollingDuration,
+                                                                   Locator locator, State state)
+    {
+        return waitActions.wait(uiContext.getSearchContext(), duration, pollingDuration,
+                state.getExpectedCondition(expectedSearchActionsConditions, locator)).isWaitPassed();
     }
 
     /**
