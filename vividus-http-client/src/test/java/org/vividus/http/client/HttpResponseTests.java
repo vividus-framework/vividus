0406292cfe693d39c4b15433619a7d7b7914039e
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-10-18 00:15:31 +0300

    Move validation of HTTP status code out of `HttpResponse`

--- vividus-http-client/src/test/java/org/vividus/http/client/HttpResponseTests.java
+++ vividus-http-client/src/test/java/org/vividus/http/client/HttpResponseTests.java
@@ -16,10 +16,8 @@
 
 package org.vividus.http.client;
 
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -29,17 +27,11 @@ import java.util.Optional;
 
 import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpStatus;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class HttpResponseTests
 {
     private static final String HEADER_NAME = "name";
-    private static final String EXCEPTION_MSG_PATTERN = "Service returned response with unexpected status code: [%d]. ";
-    private static final String EXCEPTION_MSG_IN_LIST = EXCEPTION_MSG_PATTERN + "Expected code is one of: ";
-    private static final String EXCEPTION_MSG_IN_RANGE = EXCEPTION_MSG_PATTERN + "Expected code from range [%d - %d]";
     private static final String OTHER_HEADER_NAME = "other_name";
 
     private final HttpResponse httpResponse = new HttpResponse();
@@ -111,82 +103,5 @@ class HttpResponseTests
         Header[] headers = { header };
         httpResponse.setResponseHeaders(headers);
         assertEquals(Optional.empty(), httpResponse.getHeaderByName(HEADER_NAME));
-    }
-
-    @Test
-    void testVerifyOneStatusCodeSuccess()
-    {
-        httpResponse.setStatusCode(HttpStatus.SC_OK);
-        HttpResponse actual = httpResponse.verifyStatusCode(HttpStatus.SC_OK);
-        assertThat(actual, Matchers.equalTo(httpResponse));
-    }
-
-    @Test
-    void testVerifyOneStatusCodeEmpty()
-    {
-        httpResponse.setStatusCode(HttpStatus.SC_OK);
-        ExternalServiceException exception = assertThrows(ExternalServiceException.class,
-                httpResponse::verifyStatusCode);
-        assertEquals(String.format(EXCEPTION_MSG_IN_LIST, HttpStatus.SC_OK), exception.getMessage());
-    }
-
-    @Test
-    void testVerifyOneStatusCodeNull()
-    {
-        httpResponse.setStatusCode(HttpStatus.SC_OK);
-        ExternalServiceException exception = assertThrows(ExternalServiceException.class,
-            () -> httpResponse.verifyStatusCode(null));
-        assertEquals(String.format(EXCEPTION_MSG_IN_LIST, HttpStatus.SC_OK), exception.getMessage());
-    }
-
-    @Test
-    void testVerifyNoExpectedStatusCodeFromCollection()
-    {
-        httpResponse.setStatusCode(HttpStatus.SC_ACCEPTED);
-        ExternalServiceException exception = assertThrows(ExternalServiceException.class,
-            () -> httpResponse.verifyStatusCode(HttpStatus.SC_OK, HttpStatus.SC_CREATED));
-        assertEquals(String.format(EXCEPTION_MSG_IN_LIST, HttpStatus.SC_ACCEPTED) + HttpStatus.SC_OK + ", "
-                + HttpStatus.SC_CREATED, exception.getMessage());
-    }
-
-    @Test
-    void testVerifySeveralStatusCodesSuccess()
-    {
-        httpResponse.setStatusCode(HttpStatus.SC_CREATED);
-        HttpResponse actual = httpResponse.verifyStatusCode(HttpStatus.SC_OK, HttpStatus.SC_CREATED);
-        assertThat(actual, Matchers.equalTo(httpResponse));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { HttpStatus.SC_PROCESSING, HttpStatus.SC_UNAUTHORIZED, HttpStatus.SC_GATEWAY_TIMEOUT })
-    void testVerifyStatusCodeException(int code)
-    {
-        httpResponse.setStatusCode(code);
-        String expectedMessage = String.format(EXCEPTION_MSG_IN_RANGE, code, HttpStatus.SC_OK,
-                HttpStatus.SC_BAD_REQUEST);
-        ExternalServiceException exception = assertThrows(ExternalServiceException.class,
-            () -> httpResponse.verifyStatusCodeInRange(HttpStatus.SC_OK, HttpStatus.SC_BAD_REQUEST));
-        assertEquals(expectedMessage, exception.getMessage());
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { HttpStatus.SC_OK, HttpStatus.SC_CREATED, HttpStatus.SC_BAD_REQUEST })
-    void testVerifyOneStatusCodeInRangeSuccess(Integer code)
-    {
-        httpResponse.setStatusCode(code);
-        HttpResponse actual = httpResponse.verifyStatusCodeInRange(HttpStatus.SC_OK, HttpStatus.SC_BAD_REQUEST);
-        assertThat(actual, Matchers.equalTo(httpResponse));
-    }
-
-    @ParameterizedTest
-    @ValueSource(ints = { HttpStatus.SC_PROCESSING, HttpStatus.SC_UNAUTHORIZED, HttpStatus.SC_GATEWAY_TIMEOUT })
-    void testVerifyOneStatusCodeInRangeException(int code)
-    {
-        httpResponse.setStatusCode(code);
-        String expectedMessage = String.format(EXCEPTION_MSG_IN_RANGE, code, HttpStatus.SC_OK,
-                HttpStatus.SC_BAD_REQUEST);
-        ExternalServiceException exception = assertThrows(ExternalServiceException.class,
-            () -> httpResponse.verifyStatusCodeInRange(HttpStatus.SC_OK, HttpStatus.SC_BAD_REQUEST));
-        assertEquals(expectedMessage, exception.getMessage());
     }
 }
