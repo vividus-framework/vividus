1a9232e3aa1085db16f4a1d14c63071cd97c6656
Vitali Kepin <vkepin@gmail.com>
2023-11-03 15:40:04 +0300

    [plugin-web-app] Align syntax of slider steps (#4509)

--- vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/SliderStepsTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/SliderStepsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2022 the original author or authors.
+ * Copyright 2019-2023 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,8 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 
+import java.util.Optional;
+
 import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,6 +44,9 @@ class SliderStepsTests
 {
     private static final String XPATH = ".//xpath";
     private static final String VALUE = "1";
+    private static final String SET_VALUE_JS = "arguments[0].value=arguments[1]";
+    private static final String VALUE_ATTRIBUTE = "value";
+    private static final String SLIDER_VALUE = "Slider value";
 
     @Mock
     private WebJavascriptActions javascriptActions;
@@ -55,43 +60,83 @@ class SliderStepsTests
     @InjectMocks
     private SliderSteps sliderSteps;
 
+    private Locator locator = new Locator(WebLocatorType.XPATH, XPATH);
+
     @Test
-    void setSliderValueNoSliderTest()
+    void setSliderValueNoSliderDeprecatedTest()
     {
         sliderSteps.setSliderValue(VALUE, XPATH);
         verifyNoInteractions(javascriptActions);
     }
 
     @Test
-    void setSliderValueTest()
+    void setSliderValueNoSliderTest()
+    {
+        sliderSteps.setSliderValue(VALUE, locator);
+        verifyNoInteractions(javascriptActions);
+    }
+
+    @Test
+    void setSliderValueDeprecatedTest()
     {
         WebElement webElement = mock(WebElement.class);
         mockBaseValidations("Slider to select value in", webElement);
         sliderSteps.setSliderValue(VALUE, XPATH);
-        verify(javascriptActions).executeScript("arguments[0].value=arguments[1]", webElement, VALUE);
+        verify(javascriptActions).executeScript(SET_VALUE_JS, webElement, VALUE);
     }
 
     @Test
-    void verifySliderValueTestNoSlider()
+    void setSliderValueTest()
+    {
+        WebElement webElement = mock(WebElement.class);
+        mockBaseValidationsElementExists("The slider to set the value", webElement);
+        sliderSteps.setSliderValue(VALUE, locator);
+        verify(javascriptActions).executeScript(SET_VALUE_JS, webElement, VALUE);
+    }
+
+    @Test
+    void verifySliderValueTestNoSliderDeprecated()
     {
         sliderSteps.verifySliderValue(VALUE, XPATH);
         verifyNoInteractions(softAssert);
     }
 
+    @Test
+    void verifySliderValueTestNoSlider()
+    {
+        sliderSteps.verifySliderValue(VALUE, locator);
+        verifyNoInteractions(softAssert);
+    }
+
     @SuppressWarnings("unchecked")
+    @Test
+    void verifySliderValueDeprecatedTest()
+    {
+        WebElement webElement = mock(WebElement.class);
+        mockBaseValidations("Slider to verify value in", webElement);
+        when(webElement.getAttribute(VALUE_ATTRIBUTE)).thenReturn(VALUE);
+        sliderSteps.verifySliderValue(VALUE, XPATH);
+        verify(softAssert).assertThat(eq(SLIDER_VALUE), eq(VALUE), any(IsEqual.class));
+    }
+
     @Test
     void verifySliderValueTest()
     {
         WebElement webElement = mock(WebElement.class);
-        mockBaseValidations("Slider to verify value in", webElement);
-        when(webElement.getAttribute("value")).thenReturn(VALUE);
-        sliderSteps.verifySliderValue(VALUE, XPATH);
-        verify(softAssert).assertThat(eq("Slider value"), eq(VALUE), any(IsEqual.class));
+        mockBaseValidationsElementExists("The slider to validate the value", webElement);
+        when(webElement.getAttribute(VALUE_ATTRIBUTE)).thenReturn(VALUE);
+        sliderSteps.verifySliderValue(VALUE, locator);
+        verify(softAssert).assertThat(eq(SLIDER_VALUE), eq(VALUE), any(IsEqual.class));
     }
 
     private void mockBaseValidations(String businessDescription, WebElement foundElement)
     {
         when(baseValidations.assertIfElementExists(businessDescription, new Locator(WebLocatorType.XPATH,
                 XpathLocatorUtils.getXPath(XPATH)))).thenReturn(foundElement);
+    }
+
+    private void mockBaseValidationsElementExists(String businessDescription, WebElement foundElement)
+    {
+        when(baseValidations.assertElementExists(businessDescription, locator)).thenReturn(Optional.of(foundElement));
     }
 }
