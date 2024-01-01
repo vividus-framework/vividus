e1787af64eaad8148b8e7b0f4a059529705f4870
Uladzislau Arlouski <uarlouski@gmail.com>
2024-01-01 17:33:46 +0200

    minor

--- vividus-plugin-html/src/main/java/org/vividus/html/HtmlLocatorType.java
+++ vividus-plugin-html/src/main/java/org/vividus/html/HtmlLocatorType.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ import org.jsoup.select.Elements;
 public enum HtmlLocatorType
 {
     XPATH("XPath", Document::selectXpath),
-    CSS_SELECTOR("CSS selector", Document::selectXpath);
+    CSS_SELECTOR("CSS selector", Document::select);
 
     private final String description;
     private final BiFunction<Document, String, Elements> finder;
--- vividus-plugin-html/src/test/java/org/vividus/html/HtmlLocatorTypeTests.java
+++ vividus-plugin-html/src/test/java/org/vividus/html/HtmlLocatorTypeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,7 +29,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.MockedStatic;
 
-class LocatorTypeTests
+class HtmlLocatorTypeTests
 {
     private static final String TITLE = "title";
     private static final String HTML = """
