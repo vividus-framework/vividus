8d379f3580f68746457954860980ef5553cf7319
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-02-05 12:01:56 +0200

    Address API changes in JSON schema validator

--- vividus-plugin-json/src/main/java/org/vividus/json/steps/JsonSchemaValidationSteps.java
+++ vividus-plugin-json/src/main/java/org/vividus/json/steps/JsonSchemaValidationSteps.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -59,7 +59,8 @@ public class JsonSchemaValidationSteps
         SpecVersion.VersionFlag version;
         try
         {
-            version = SpecVersionDetector.detectOptionalVersion(schemaNode).orElse(SpecVersion.VersionFlag.V202012);
+            version = SpecVersionDetector.detectOptionalVersion(schemaNode, true)
+                    .orElse(SpecVersion.VersionFlag.V202012);
         }
         catch (JsonSchemaException e)
         {
