e9cdba1d6fe9f6e5c98df7477fecc4b183bdc342
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-02-05 18:02:13 +0200

    [util] Simplify `Sleeper` (#4798)

--- vividus-util/src/main/java/org/vividus/util/Sleeper.java
+++ vividus-util/src/main/java/org/vividus/util/Sleeper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2020 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,7 +34,7 @@ public final class Sleeper
     {
         try
         {
-            Thread.sleep(TimeUnit.MILLISECONDS.convert(time, timeUnit));
+            timeUnit.sleep(time);
         }
         catch (InterruptedException e)
         {
