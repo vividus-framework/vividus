79137c23aa432dd7ed08a9225c1f5745eaef931e
Uladzislau Arlouski <uarlouski@gmail.com>
2024-02-13 15:37:17 +0200

    Consider sticky header size when scrolling element into view

--- vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/ScrollStepsTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/ScrollStepsTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2024 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -126,14 +126,14 @@ class ScrollStepsTests
     }
 
     @Test
-    void shouldScrollElementIntoViewAlignedToATop()
+    void shouldScrollElementIntoView()
     {
         var webElement = mock(WebElement.class);
         var locator = mock(Locator.class);
         when(baseValidations.assertIfElementsExist(ELEMENT_TO_SCROLL_INTO_VIEW, locator)).thenReturn(
                 List.of(webElement));
         scrollSteps.scrollIntoView(locator);
-        verify(javascriptActions).scrollIntoView(webElement, true);
+        verify(javascriptActions).scrollElementIntoViewportCenter(webElement);
     }
 
     @Test
