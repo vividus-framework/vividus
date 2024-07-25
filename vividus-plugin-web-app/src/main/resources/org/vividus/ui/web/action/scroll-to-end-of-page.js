24276df40f899543ffe535742b9e5cc8b35bb49c
Valery Yatsynovich <valery_yatsynovich@epam.com>
2024-07-25 11:12:38 +0300

    [plugin-web-app] Fix infinite loops in script scrolling to the end of page

--- vividus-plugin-web-app/src/main/resources/org/vividus/ui/web/action/scroll-to-end-of-page.js
+++ vividus-plugin-web-app/src/main/resources/org/vividus/ui/web/action/scroll-to-end-of-page.js
@@ -1,9 +1,12 @@
-var exit = arguments[arguments.length-1];
+const exit = arguments[arguments.length-1];
+const maxNumberOfScrolls = 10;
+let currentScrollIteration = 0;
 (scrollToEndOfPage = function() {
     bottom = document.body.scrollHeight;
     current = window.innerHeight + Math.round(window.scrollY);
-    if((bottom - current) > 0) { 
+    if((bottom - current) > 0 && (currentScrollIteration < maxNumberOfScrolls)) {
         window.scrollTo(0, bottom);
+        currentScrollIteration++;
         setTimeout('scrollToEndOfPage()', 500);
     } else {
         exit();
