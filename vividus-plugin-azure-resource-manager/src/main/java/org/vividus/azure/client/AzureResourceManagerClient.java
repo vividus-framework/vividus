aa556e46dacef583a3c33ac63f5b328388e2b6f2
Aliona Budzevich <aliona_budzevich@epam.com>
2024-10-04 10:51:43 +0200

    [plugin-azure-key-vault] Add ability to read property values from Azure KeyVault

---
+++ vividus-plugin-azure-resource-manager/src/main/java/org/vividus/azure/client/AzureResourceManagerClient.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2019-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.vividus.azure.client;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.ContentType;
+import com.azure.core.http.HttpHeaderName;
+import com.azure.core.http.HttpMethod;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.HttpRequest;
+import com.azure.core.http.HttpResponse;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.resourcemanager.resources.fluentcore.utils.HttpPipelineProvider;
+
+import org.apache.commons.lang3.StringUtils;
+import org.vividus.softassert.ISoftAssert;
+
+import io.netty.handler.codec.http.HttpResponseStatus;
+import reactor.core.publisher.Mono;
+
+public class AzureResourceManagerClient
+{
+    private static final String EXECUTION_FAILED = "Azure REST API HTTP request execution is failed";
+
+    private final HttpPipeline httpPipeline;
+    private final AzureProfile azureProfile;
+    private final ISoftAssert softAssert;
+
+    public AzureResourceManagerClient(AzureProfile azureProfile, TokenCredential tokenCredential,
+                                      ISoftAssert softAssert)
+    {
+        this.azureProfile = azureProfile;
+        this.httpPipeline = HttpPipelineProvider.buildHttpPipeline(tokenCredential, azureProfile);
+        this.softAssert = softAssert;
+    }
+
+    /**
+     * Executes an HTTP request to the specified Azure resource using the provided parameters.
+     * This method constructs the URL for the request, sends the HTTP request, and handles the response
+     * or errors using the provided Consumer and ISoftAssert.
+     *
+     * @param method                  The HTTP method to be used for the request (e.g., GET, POST, PUT, DELETE).
+     * @param azureResourceIdentifier This is a VIVIDUS-only term. It's used to specify Azure resource uniquely. From
+     *                                the technical perspective it's a part of Azure resource REST API URL path. For
+     *                                example, if the full Azure resource URL is
+     *                                <br>
+     *                                <code>https://management.azure.com/subscriptions/
+     *                                00000000-0000-0000-0000-000000000000/resourceGroups/sample-resource-group/
+     *                                providers/Microsoft.KeyVault/vaults/sample-vault?api-version=2021-10-01</code>,
+     *                                <br>
+     *                                then resource identifier will be
+     *                                <br>
+     *                                <code>resourceGroups/sample-resource-group/providers/Microsoft.KeyVault/
+     *                                vaults/sample-vault</code>.
+     * @param apiVersion              Azure resource provider API version. Note API versions may vary depending on
+     *                                the resource type.
+     * @param azureResourceBody       The Azure resource configuration in JSON format.
+     * @param responseBodyConsumer    The Consumer that handles the response body if the request is successful.
+     */
+    public void executeHttpRequest(HttpMethod method, String azureResourceIdentifier, String apiVersion,
+                                   Optional<String> azureResourceBody, Consumer<String> responseBodyConsumer)
+    {
+        String url = String.format("%ssubscriptions/%s%s?api-version=%s",
+                azureProfile.getEnvironment().getResourceManagerEndpoint(), azureProfile.getSubscriptionId(),
+                StringUtils.prependIfMissing(azureResourceIdentifier, "/"), apiVersion);
+        executeHttpRequest(method, url, azureResourceBody, responseBodyConsumer, softAssert::recordFailedAssertion);
+    }
+
+    /**
+     * Executes an HTTP request to the specified URL using the provided parameters.
+     * This method allows for flexible error handling based on the availability of a soft assertion mechanism.
+     * If softAssert is available, it uses soft assertions to handle errors; otherwise,
+     * it throws RuntimeException on errors.
+     *
+     * @param method               The HTTP method to be used for the request (e.g., GET, POST, PUT, DELETE).
+     * @param azureResourceUrl     It's used to specify Azure resource uniquely. For example:
+     *                             <ul>
+     *                             <li><code>https://management.azure.com/subscriptions/
+     *                             00000000-0000-0000-0000-000000000000/resourceGroups/sample-resource-group/
+     *                             providers/Microsoft.KeyVault/vaults/sample-vault?api-version=2021-10-01</code>
+     *                             <br><i>or</i>
+     *                             <br>
+     *                             <li><code>https://api.loganalytics.io/v1/workspaces/00000000-0000-0000-0000-000000000000/
+     *                             query?query=Syslog</code>
+     *                             </ul>
+     * @param azureResourceBody    The Azure resource configuration in JSON format.
+     * @param responseBodyConsumer The Consumer that handles the response body if the request is successful.
+     */
+    public void executeHttpRequest(HttpMethod method, String azureResourceUrl, Optional<String> azureResourceBody,
+                                    Consumer<String> responseBodyConsumer)
+    {
+        Consumer<String> errorCallback = error -> { throw new RuntimeException(error); };
+        if (softAssert != null)
+        {
+            errorCallback = softAssert::recordFailedAssertion;
+        }
+        executeHttpRequest(method, azureResourceUrl, azureResourceBody, responseBodyConsumer, errorCallback);
+    }
+
+    private void executeHttpRequest(HttpMethod method, String azureResourceUrl, Optional<String> azureResourceBody,
+                                   Consumer<String> responseBodyConsumer, Consumer<String> errorCallback)
+    {
+        HttpRequest httpRequest = new HttpRequest(method, azureResourceUrl);
+        azureResourceBody.ifPresent(requestBody -> {
+            httpRequest.setBody(requestBody);
+            httpRequest.setHeader(HttpHeaderName.CONTENT_TYPE, ContentType.APPLICATION_JSON);
+        });
+
+        try (HttpResponse httpResponse = httpPipeline.send(httpRequest).block())
+        {
+            Optional.ofNullable(httpResponse).map(HttpResponse::getBodyAsString).map(Mono::block).ifPresentOrElse(
+                    responseBody -> {
+                        if (httpResponse.getStatusCode() == HttpResponseStatus.OK.code())
+                        {
+                            responseBodyConsumer.accept(responseBody);
+                        }
+                        else
+                        {
+                            errorCallback.accept(EXECUTION_FAILED + ": " + responseBody);
+                        }
+                    }, () -> errorCallback.accept(EXECUTION_FAILED + " with empty body and status code: "
+                            + httpResponse.getStatusCode()
+                    )
+            );
+        }
+    }
+}
