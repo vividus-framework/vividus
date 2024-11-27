47adca63428ad68babd7e93003410711f1674db0
Uladzislau Arlouski <uarlouski@gmail.com>
2024-11-27 16:46:07 +0200

    [plugin-web-app] Automatically grant permission to track geolocation (#5548)

--- vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/devtools/GeolocationEmulationStepsTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/steps/ui/web/devtools/GeolocationEmulationStepsTests.java
@@ -16,36 +16,76 @@
 
 package org.vividus.steps.ui.web.devtools;
 
+import static com.github.valfirst.slf4jtest.LoggingEvent.info;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+
+import com.github.valfirst.slf4jtest.LoggingEvent;
+import com.github.valfirst.slf4jtest.TestLogger;
+import com.github.valfirst.slf4jtest.TestLoggerFactory;
+import com.github.valfirst.slf4jtest.TestLoggerFactoryExtension;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.vividus.selenium.cdp.BrowserPermissions;
+import org.vividus.selenium.manager.WebDriverManager;
 import org.vividus.ui.web.action.CdpActions;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({ MockitoExtension.class, TestLoggerFactoryExtension.class })
 class GeolocationEmulationStepsTests
 {
+    private final TestLogger testLogger = TestLoggerFactory.getTestLogger(GeolocationEmulationSteps.class);
+
     @Mock private CdpActions cdpActions;
+    @Mock private WebDriverManager webDriverManager;
     @InjectMocks private GeolocationEmulationSteps steps;
 
-    @Test
-    void shouldEmulateGeolocation()
+    static Stream<Arguments> geolocationState()
     {
+        return Stream.of(
+            arguments(false, 1, List.of(info("The browser has been granted the permission to track geolocation"))),
+            arguments(true, 0, List.of())
+        );
+    }
+
+    @MethodSource("geolocationState")
+    @ParameterizedTest
+    void shouldEmulateGeolocation(boolean initialState, int permissionSetInvocations, List<LoggingEvent> messages)
+    {
+        BrowserPermissions permissions = new BrowserPermissions();
+        permissions.setGeolocationEnabled(initialState);
+        when(webDriverManager.getBrowserPermissions()).thenReturn(permissions);
+
         double latitude = 55.410014;
         double longitude = 28.628027;
 
         steps.emulateGeolocation(latitude, longitude);
 
+        verify(cdpActions, times(permissionSetInvocations)).executeCdpCommand("Browser.grantPermissions", Map.of(
+                "permissions", List.of("geolocation")
+        ));
         verify(cdpActions).executeCdpCommand("Emulation.setGeolocationOverride", Map.of(
                 "latitude", latitude,
                 "longitude", longitude,
                 "accuracy", 1
         ));
+        assertTrue(permissions.isGeolocationEnabled());
+        assertThat(testLogger.getLoggingEvents(), is(messages));
     }
 
     @Test
