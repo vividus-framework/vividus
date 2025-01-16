bbe74ebd88c6cffdc30c164a881c746984851b97
Valery Yatsynovich <valery_yatsynovich@epam.com>
2025-01-16 22:41:02 +0300

    Remove redundant suppressions of SpotBugs violations

--- vividus-plugin-web-app/src/test/java/org/vividus/selenium/WebContextSourceCodeProviderTests.java
+++ vividus-plugin-web-app/src/test/java/org/vividus/selenium/WebContextSourceCodeProviderTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 the original author or authors.
+ * Copyright 2019-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +69,6 @@ class WebContextSourceCodeProviderTests
 
     private final TestLogger logger = TestLoggerFactory.getTestLogger(WebContextSourceCodeProvider.class);
 
-    @SuppressFBWarnings("VA_FORMAT_STRING_USES_NEWLINE")
     @Test
     void shouldReturnWholePageForDriverContext()
     {
