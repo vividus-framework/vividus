869b656b53c98f22bc9dd8e1f828a2601972e1e9
vkepin <vkepin@gmail.com>
2025-02-05 16:05:26 +0100

    [plugin-saucelabs] Add waiting for SauceLabs job completion after Story execution

--- vividus-plugin-saucelabs/src/main/java/org/vividus/saucelabs/SauceLabsTestStatusManager.java
+++ vividus-plugin-saucelabs/src/main/java/org/vividus/saucelabs/SauceLabsTestStatusManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 the original author or authors.
+ * Copyright 2019-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,26 +16,77 @@
 
 package org.vividus.saucelabs;
 
+import java.io.IOException;
+
+import com.google.common.eventbus.Subscribe;
+import com.saucelabs.saucerest.SauceREST;
+import com.saucelabs.saucerest.model.jobs.Job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vividus.selenium.IWebDriverProvider;
 import org.vividus.selenium.cloud.AbstractCloudTestStatusManager;
 import org.vividus.selenium.cloud.CloudTestStatusMapping;
+import org.vividus.selenium.event.AfterWebDriverQuitEvent;
 import org.vividus.testcontext.TestContext;
 import org.vividus.ui.action.JavascriptActions;
+import org.vividus.util.wait.Waiter;
 
 public class SauceLabsTestStatusManager extends AbstractCloudTestStatusManager
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SauceLabsTestStatusManager.class);
+
+    private static final long SESSION_COMPLETE_TIMEOUT_SECONDS = 20;
+
     private final JavascriptActions javascriptActions;
+    private final SauceREST sauceRestClient;
+    private final Waiter sessionCompletionWaiter;
 
     public SauceLabsTestStatusManager(IWebDriverProvider webDriverProvider, TestContext testContext,
-            JavascriptActions javascriptActions)
+            JavascriptActions javascriptActions, SauceREST sauceRestClient, Waiter sessionCompletionWaiter)
     {
         super(new CloudTestStatusMapping("passed", "failed"), webDriverProvider, testContext);
         this.javascriptActions = javascriptActions;
+        this.sauceRestClient = sauceRestClient;
+        this.sessionCompletionWaiter = sessionCompletionWaiter;
     }
 
     @Override
     protected void updateCloudTestStatus(String sessionId, String status) throws UpdateCloudTestStatusException
     {
         javascriptActions.executeScript("sauce:job-result=" + status);
+    }
+
+    @Subscribe
+    public final void waitForSessionCompletion(AfterWebDriverQuitEvent event)
+    {
+        String sessionId = event.sessionId();
+        boolean completed = sessionCompletionWaiter.wait(() -> isTestExecutionCompleted(sessionId), r -> r);
+        if (!completed)
+        {
+            LOGGER.atError().addArgument(SESSION_COMPLETE_TIMEOUT_SECONDS)
+                    .addArgument(sessionId)
+                    .log("Timeout while waiting for SauceLabs test job completion after {} seconds. SessionId: {}");
+        }
+    }
+
+    private boolean isTestExecutionCompleted(String sessionId)
+    {
+        try
+        {
+            Job jobDetails = sauceRestClient.getJobsEndpoint().getJobDetails(sessionId);
+            String jobStatus = jobDetails.status;
+            LOGGER.atDebug().addArgument(sessionId)
+                    .addArgument(jobStatus)
+                    .log("SauceLabs test with id={} execution status is {}");
+            return "complete".equals(jobStatus);
+        }
+        catch (IOException e)
+        {
+            LOGGER.atError().setCause(e)
+                    .addArgument(sessionId)
+                    .log("Unable to get SauceLabs test job status for SessionId={}");
+        }
+        return false;
     }
 }
