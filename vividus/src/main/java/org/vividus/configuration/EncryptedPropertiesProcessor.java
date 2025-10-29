089aec15185e0fca857bdb26047561a3ec05a1b2
Aliona Budzevich <aliona_budzevich@epam.com>
2025-10-29 17:32:32 +0100

    Add ability to disable secret manager processors

--- vividus/src/main/java/org/vividus/configuration/EncryptedPropertiesProcessor.java
+++ vividus/src/main/java/org/vividus/configuration/EncryptedPropertiesProcessor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 the original author or authors.
+ * Copyright 2019-2025 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,6 +31,12 @@ public class EncryptedPropertiesProcessor extends AbstractPropertiesProcessor
     public EncryptedPropertiesProcessor()
     {
         super(ENC);
+    }
+
+    @Override
+    protected boolean isEnabled(Properties properties)
+    {
+        return true;
     }
 
     @Override
