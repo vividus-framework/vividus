aec93e25159d9fd57f57cbf414a8e8a8dab6212e
Valery Yatsynovich <valery_yatsynovich@epam.com>
2026-08-07 14:50:16 +0300

    [plugin-mobile-app] Fix usage of Proxy steps for mobile apps
    The fixed error:
    org.jbehave.core.steps.ParameterConverters$ParameterConversionFailed: No parameter converter for class io.netty.handler.codec.http.DefaultHttpHeaders
      at org.jbehave.core.steps.ParameterConverters.convert(ParameterConverters.java:419)
      at org.jbehave.core.steps.ParameterConverters.convert(ParameterConverters.java:360)
      at org.vividus.steps.ParameterConvertersDecorator.convertAdaptedValue(ParameterConvertersDecorator.java:81)
      at org.vividus.steps.ParameterConvertersDecorator.convert(ParameterConvertersDecorator.java:54)
      at org.jbehave.core.steps.StepCreator$ParametrisedStep.convertParameterValues(StepCreator.java:996)
      at org.jbehave.core.steps.StepCreator$ParametrisedStep.parametriseStep(StepCreator.java:980)
      at org.jbehave.core.steps.StepCreator$ParametrisedStep.perform(StepCreator.java:912)
      at org.jbehave.core.steps.StepCreator$ReportingAbstractStep.perform(StepCreator.java:644)
      at org.jbehave.core.embedder.PerformableTree$FineSoFar.run(PerformableTree.java:380)
      at org.jbehave.core.embedder.PerformableTree$PerformableSteps.perform(PerformableTree.java:1454)
      at org.jbehave.core.embedder.PerformableTree$AbstractPerformableScenario.lambda$performScenario$0(PerformableTree.java:1296)
      at org.jbehave.core.embedder.PerformableTree$PerformableEntity.performWithStopExecutionExceptionHandling(PerformableTree.java:976)
      at org.jbehave.core.embedder.PerformableTree$AbstractPerformableScenario.performScenario(PerformableTree.java:1289)
      at org.jbehave.core.embedder.PerformableTree$NormalPerformableScenario.perform(PerformableTree.java:1345)
      at org.jbehave.core.embedder.PerformableTree$PerformableScenario.perform(PerformableTree.java:1238)
      at org.jbehave.core.embedder.PerformableTree$PerformableStory.lambda$perform$0(PerformableTree.java:1101)
      at org.jbehave.core.embedder.PerformableTree$PerformableEntity.performWithStopExecutionExceptionHandling(PerformableTree.java:976)
      at org.jbehave.core.embedder.PerformableTree$PerformableStory.perform(PerformableTree.java:1093)
      at org.jbehave.core.embedder.PerformableTree.performCancellable(PerformableTree.java:531)
      at org.jbehave.core.embedder.PerformableTree.perform(PerformableTree.java:498)
      at org.jbehave.core.embedder.StoryManager$EnqueuedStory.call(StoryManager.java:305)
      at org.jbehave.core.embedder.StoryManager$EnqueuedStory.call(StoryManager.java:278)
      at java.base/java.util.concurrent.FutureTask.run(FutureTask.java:317)
      at java.base/java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1144)
      at java.base/java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:642)
      at java.base/java.lang.Thread.run(Thread.java:1583)

--- vividus-extension-selenium/src/test/java/org/vividus/ui/converter/ExamplesTableToDefaultHttpHeadersConverterTests.java
+++ vividus-extension-selenium/src/test/java/org/vividus/ui/converter/ExamplesTableToDefaultHttpHeadersConverterTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2024 the original author or authors.
+ * Copyright 2019-2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.vividus.ui.web.converter;
+package org.vividus.ui.converter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
